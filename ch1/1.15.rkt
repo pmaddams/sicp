160a61d76026125ab992e41e18e5f13be3ef9509
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-19 19:13:52 -0400

    1.15

---
+++ ch1/1.15.rkt
@@ -0,0 +1,21 @@
+#lang sicp
+
+(define (sine angle)
+  (let ((p (lambda (x)
+             (- (* 3 x)
+                (* 4 (expt x 3))))))
+    (if (<= (abs angle) 0.1)
+        angle
+        (p (sine (/ angle 3.0))))))
+
+;; (sine 12.15)
+;; (p (sine 4.05))
+;; (p (p (sine 1.35)))
+;; (p (p (p (sine 0.45))))
+;; (p (p (p (p (sine 0.15)))))
+;; (p (p (p (p (p (sine 0.05))))))
+;; (p (p (p (p (p (0.05))))))
+
+;; When evaluating (sine 12.15), p is applied 5 times.
+
+;; The order of growth with respect to both space and time is theta of log(a).
