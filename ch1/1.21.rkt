afd76f3e1c753368562de4bd27ef2d950d7bbf1e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-21 19:54:16 -0400

    don't use unnecessary argument

--- ch1/1.21.rkt
+++ ch1/1.21.rkt
@@ -5,11 +5,11 @@
                      (expt x 2)))
            (divides? (lambda (a b)
                        (zero? (remainder b a))))
-           (find-divisor (lambda (n test-divisor)
+           (find-divisor (lambda (test-divisor)
                            (cond ((> (square test-divisor) n) n)
                                  ((divides? test-divisor n) test-divisor)
-                                 (else (find-divisor n (inc test-divisor)))))))
-    (find-divisor n 2)))
+                                 (else (find-divisor (inc test-divisor)))))))
+    (find-divisor 2)))
 
 (smallest-divisor 199)
 ;; 199
