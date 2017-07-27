d5fabdb8f33b9cdc143b00616b17cd3f023417a3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-26 20:07:14 -0400

    1.37

--- ch1/1.37.rkt
+++ ch1/1.37.rkt
@@ -15,3 +15,20 @@
                     (c (dec i) (/ (n i)
                                   (+ (d i) result)))))))
     (c k 0)))
+
+(define (how-large-must-k-be? f)
+  (letrec ((phi 1.6180)
+           (one (lambda (x) 1.0))
+           (ok? (lambda (x)
+                  (< (abs (- (/ 1 x) phi)) 0.0001)))
+           (o (lambda (k)
+                (if (ok? (f one one k))
+                    k
+                    (o (inc k))))))
+    (o 1)))
+
+(how-large-must-k-be? cont-frac-rec)
+;; 11
+
+(how-large-must-k-be? cont-frac-iter)
+;; 11
