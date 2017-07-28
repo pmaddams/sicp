389937d93193f10a1b5f96e36a944eb5bb22c943
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-27 21:51:16 -0400

    use expt

--- ch1/1.16.rkt
+++ ch1/1.16.rkt
@@ -2,9 +2,13 @@
 
 (define (fast-expt-iter b n)
   (letrec ((square (lambda (x)
-                     (* x x)))
+                     (expt x 2)))
            (f (lambda (a b n)
-                        (cond ((zero? n) a)
-                              ((even? n) (f a (square b) (/ n 2)))
-                              (else (f (* a b) b (dec n)))))))
+                (cond ((zero? n) a)
+                      ((even? n) (f a (square b) (/ n 2)))
+                      (else (f (* a b) b (dec n)))))))
     (f 1 b n)))
+
+(let ((expt fast-expt-iter))
+  (expt 2 64))
+;; 18446744073709551616
