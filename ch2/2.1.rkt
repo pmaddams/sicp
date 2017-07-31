9dc6614ef7cdb4afdf33b4bccac634a2cfab40c7
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-30 20:41:30 -0400

    2.1

---
+++ ch2/2.1.rkt
@@ -0,0 +1,44 @@
+#lang sicp
+
+(define (gcd a b)
+  (if (= b 0)
+      a
+      (gcd b (remainder a b))))
+
+(define (make-rat n d)
+  (let ((g (gcd n d)))
+    (begin (set! n (/ n g))
+           (set! d (/ d g))
+           (if (negative? d)
+               (cons (- n) (- d))
+               (cons n d)))))
+
+(define (numer x)
+  (car x))
+
+(define (denom x)
+  (cdr x))
+
+(define (print-rat x)
+  (newline)
+  (display (numer x))
+  (display "/")
+  (display (denom x)))
+
+(define (add-rat x y)
+  (make-rat (+ (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (sub-rat x y)
+  (make-rat (- (* (numer x) (denom y))
+               (* (numer y) (denom x)))
+            (* (denom x) (denom y))))
+
+(define (mul-rat x y)
+  (make-rat (* (numer x) (numer y))
+            (* (denom x) (denom y))))
+
+(define (div-rat x y)
+  (make-rat (* (numer x) (denom y))
+            (* (denom x) (numer x))))
