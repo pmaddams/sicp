3703b9acb292927c228b3c359b59492a19ed8893
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-31 20:44:48 -0400

    2.6

---
+++ ch2/2.6.rkt
@@ -0,0 +1,43 @@
+#lang sicp
+
+(define zero
+  (lambda (f)
+    (lambda (x)
+      x)))
+
+(define (add1 n)
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+
+(add1 zero)
+(lambda (f)
+  (lambda (x)
+    (f ((zero f) x))))
+(lambda (f)
+  (lambda (x)
+    (f ((lambda (x) x) x))))
+(lambda (f)
+  (lambda (x)
+    (f x)))
+
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f x))))
+
+(add1 one)
+(lambda (f)
+  (lambda (x)
+    (f ((one f) x))))
+(lambda (f)
+  (lambda (x)
+    (f ((lambda (x) (f x)) x))))
+(lambda (f)
+  (lambda (x)
+    (f (f x))))
+
+(define two
+  (lambda (f)
+    (lambda (x)
+      (f (f x)))))
