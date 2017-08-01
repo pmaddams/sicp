8afb4054629164fe408830f34c0561f2afd5979d
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-07-31 21:49:01 -0400

    2.8

---
+++ ch2/2.8.rkt
@@ -0,0 +1,37 @@
+#lang sicp
+
+;; Let i - j = k, where i, j, and k are intervals. The lower bound of k is equal
+;; to the lower bound of i minus the upper bound of j, and the upper bound of k 
+;; is equal to the upper bound of i minus the lower bound of j.
+
+(define make-interval cons)
+
+(define lower-bound car)
+
+(define upper-bound cdr)
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (mul-interval x y)
+  (let ((p1 (* (lower-bound x) (lower-bound y)))
+        (p2 (* (lower-bound x) (upper-bound y)))
+        (p3 (* (upper-bound x) (lower-bound y)))
+        (p4 (* (upper-bound x) (upper-bound y))))
+    (make-interval (min p1 p2 p3 p4)
+                   (max p1 p2 p3 p4))))
+
+(define (div-interval x y)
+  (mul-interval x 
+                (make-interval (/ 1.0 (upper-bound y))
+                               (/ 1.0 (lower-bound y)))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(let ((i (make-interval 1 2))
+      (j (make-interval 3 4)))
+  (display (sub-interval i j)))
+;; (-3 . -1)
