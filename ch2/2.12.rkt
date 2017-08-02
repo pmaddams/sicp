86dedc31a40cc6b7a1357dc924a69248b9e01954
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-02 18:44:21 -0400

    2.12

---
+++ ch2/2.12.rkt
@@ -0,0 +1,61 @@
+#lang sicp
+
+(define make-interval cons)
+
+(define lower-bound car)
+
+(define upper-bound cdr)
+
+(define (add-interval x y)
+  (make-interval (+ (lower-bound x) (lower-bound y))
+                 (+ (upper-bound x) (upper-bound y))))
+
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+                 (- (upper-bound x) (lower-bound y))))
+
+(define (mul-interval x y)
+  (let ((x1 (lower-bound x))
+        (x2 (upper-bound x))
+        (y1 (lower-bound y))
+        (y2 (upper-bound y))
+        (nonneg? (lambda (n)
+                   (or (zero? n)
+                       (positive? n)))))
+    (cond ((nonneg? x1)
+           (cond ((nonneg? y1) (make-interval (* x1 y1)
+                                              (* x2 y2)))
+                 ((negative? y2) (make-interval (* x2 y1)
+                                                (* x1 y2)))
+                 (else (make-interval (* x2 y1)
+                                      (* x2 y2)))))
+          ((negative? x2)
+           (cond ((nonneg? y1) (make-interval (* x1 y2)
+                                              (* x2 y1)))
+                 ((negative? y2) (make-interval (* x2 y2)
+                                                (* x1 y1)))
+                 (else (make-interval (* x1 y2)
+                                      (* x1 y1)))))
+          (else
+           (cond ((nonneg? y1) (make-interval (* x1 y2)
+                                              (* x2 y2)))
+                 ((negative? y2) (make-interval (* x2 y1)
+                                                (* x1 y1)))
+                 (else (make-interval (let ((a (* x1 y2))
+                                            (b (* x2 y1)))
+                                        (if (< a b)
+                                            a
+                                            b))
+                                      (let ((a (* x1 y1))
+                                            (b (* x2 y2)))
+                                        (if (> a b)
+                                            a
+                                            b)))))))))
+
+(define (div-interval x y)
+  (if (or (zero? (lower-bound y))
+          (zero? (upper-bound x)))
+      (error "division by zero")
+      (mul-interval x
+                    (make-interval (/ 1.0 (upper-bound y))
+                                   (/ 1.0 (lower-bound y))))))
