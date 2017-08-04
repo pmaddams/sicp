8f033758561f5b37313c3eaffee1a439cc3a120f
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-04 19:25:06 -0400

    2.27

---
+++ ch2/2.27.rkt
@@ -0,0 +1,13 @@
+#lang sicp
+
+(define (deep-reverse l)
+  (letrec ((r (lambda (l newl)
+                (cond ((null? l) newl)
+                      ((list? (car l)) (r (cdr l) (cons (r (car l) '()) newl)))
+                      (else (r (cdr l) (cons (car l) newl)))))))
+    (r l '())))
+
+(let ((l '((1 2) (3 4))))
+  (begin (display (deep-reverse l))
+         (newline)))
+;; ((4 3) (2 1))
