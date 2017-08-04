ab3b1ddfaffbff4d720024e812df7b6f58964640
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-04 19:26:46 -0400

    use better names

--- ch2/2.18.rkt
+++ ch2/2.18.rkt
@@ -1,12 +1,13 @@
 #lang sicp
 
 (define (reverse l)
-  (letrec ((r (lambda (l1 l2)
-                (if (null? l1)
-                    l2
-                    (r (cdr l1) (cons (car l1) l2))))))
+  (letrec ((r (lambda (l newl)
+                (if (null? l)
+                    newl
+                    (r (cdr l) (cons (car l) newl))))))
     (r l '())))
 
 (let ((l '(1 4 9 16 25)))
-  (display (reverse l)))
+  (begin (display (reverse l))
+         (newline)))
 ;; (25 16 9 4 1)
