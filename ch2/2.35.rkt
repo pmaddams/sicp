c048a118d33f139c2a0d55870ebb3486057a31d3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-07 21:29:48 -0400

    2.35

---
+++ ch2/2.35.rkt
@@ -0,0 +1,27 @@
+#lang sicp
+
+(define (accumulate op init seq)
+  (letrec ((a (lambda (seq)
+                (if (null? seq)
+                    init
+                    (op (car seq) (a (cdr seq)))))))
+    (a seq)))
+
+(define (count-leaves t)
+  (accumulate +
+              0
+              (map (lambda (st)
+                     (if (not (pair? st))
+                         1
+                         (count-leaves st)))
+                   t)))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(let ((t '((1 2) (3 4))))
+  (displayln (count-leaves t))
+  (displayln (count-leaves (list t t))))
+;; 4
+;; 8
