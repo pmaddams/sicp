d5ed25b9481f2605e5896c62e2a05f7f10e0929b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-07 20:45:46 -0400

    2.33

--- ch2/2.33.rkt
+++ ch2/2.33.rkt
@@ -1,35 +1,42 @@
 #lang sicp
 
-(define (square x)
-  (expt x 2))
-
-(define (fib n)
-  (letrec ((f (lambda (a b p q count)
-                (cond ((zero? count) b)
-                      ((even? count) (f a
-                                        b
-                                        (+ (expt p 2)
-                                           (expt q 2))
-                                        (+ (* 2 p q)
-                                           (expt q 2))
-                                        (/ count 2)))
-                      (else (f (+ (* b q) (* a q) (* a p))
-                               (+ (* b p) (* a q))
-                               p
-                               q
-                               (dec count)))))))
-    (f 1 0 0 1 n)))
-
-(define (filter pred? seq)
-  (letrec ((f (lambda (seq)
-                (cond ((null? seq) '())
-                      ((pred? (car seq)) (cons (car seq) (f (cdr seq))))
-                      (else (f (cdr seq)))))))
-    (f seq)))
-
 (define (accumulate op init seq)
   (letrec ((a (lambda (seq)
                 (if (null? seq)
                     init
                     (op (car seq) (a (cdr seq)))))))
     (a seq)))
+
+(define (map p seq)
+  (accumulate (lambda (x y)
+                (cons (p x) y))
+              '()
+              seq))
+
+(define (append seq1 seq2)
+  (accumulate cons seq2 seq1))
+
+(define (length seq)
+  (accumulate (lambda (x y)
+                (inc y))
+              0
+              seq))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(let ((square (lambda (x)
+                (expt x 2)))
+      (seq '(1 2 3 4 5)))
+  (displayln (map square seq)))
+;; (1 4 9 16 25)
+
+(let ((seq1 '(1 2 3 4 5))
+      (seq2 '(6 7 8 9 10)))
+  (displayln (append seq1 seq2)))
+;; (1 2 3 4 5 6 7 8 9 10)
+
+(let ((seq '(a b c d e)))
+  (displayln (length seq)))
+;; 5
