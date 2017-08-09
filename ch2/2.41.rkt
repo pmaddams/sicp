29f5fe5bae0dfde89ff40d37916760b07e89f43b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-08 21:23:26 -0400

    2.41

---
+++ ch2/2.41.rkt
@@ -0,0 +1,53 @@
+#lang sicp
+
+(define (accumulate op init seq)
+  (letrec ((a (lambda (seq)
+                (if (null? seq)
+                    init
+                    (op (car seq) (a (cdr seq)))))))
+    (a seq)))
+
+(define (filter pred? seq)
+  (letrec ((f (lambda (seq)
+                (cond ((null? seq) '())
+                      ((pred? (car seq)) (cons (car seq) (f (cdr seq))))
+                      (else (f (cdr seq)))))))
+    (f seq)))
+
+(define (enumerate-interval i lim)
+  (letrec ((e (lambda (i)
+                (if (> i lim)
+                    '()
+                    (cons i (e (inc i)))))))
+    (e i)))
+
+(define (flatmap p seq)
+  (accumulate append '() (map p seq)))
+
+(define (triples-sum-to-s s n)
+  (let ((triples (lambda ()
+                   (flatmap (lambda (i)
+                              (flatmap (lambda (j)
+                                         (map (lambda (k) (list i j k))
+                                              (enumerate-interval 1 n)))
+                                       (enumerate-interval 1 n)))
+                            (enumerate-interval 1 n))))
+        (sum-to-s? (lambda (t)
+                     (= s (+ (car t) (cadr t) (caddr t))))))
+    (filter sum-to-s? (triples))))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(for-each displayln (triples-sum-to-s 6 6))
+;; (1 1 4)
+;; (1 2 3)
+;; (1 3 2)
+;; (1 4 1)
+;; (2 1 3)
+;; (2 2 2)
+;; (2 3 1)
+;; (3 1 2)
+;; (3 2 1)
+;; (4 1 1)
