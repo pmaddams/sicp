ac9edcce5323da6eee72a828bb62ef848244edd7
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-13 19:04:21 -0400

    2.64

--- ch2/2.64.rkt
+++ ch2/2.64.rkt
@@ -66,6 +66,20 @@
                              remaining-l))))))
     (lt l (length l))))
 
+;; The partial-tree procedure, here locally defined as lt, has a recursive base
+;; case where n, the number of elements in the subtree, is zero. We descend the
+;; tree leftwards, floor dividing the number of elements by 2 repeatedly until n
+;; becomes zero, to reach the correct depth for the final tree. Now we take the
+;; cons of the empty list and the list of elements to be the left result, and
+;; the car, which is now the empty list, to be the left tree. The second element
+;; of the list becomes the node entry. The right subtree is then constructed
+;; from the car of another call to partial-tree at the same depth. We generate a
+;; node using make-tree, from the current node entry, the left subtree, and the
+;; right subtree. This node is then consed to the result of the right subtree
+;; from the node above. We continue walking up the tree until the list of
+;; remaining elements is empty, at which point we have generated the entire
+;; balanced tree structure.
+
 (define (displayln x)
   (display x)
   (newline))
@@ -79,3 +93,15 @@
 ;; (1 () (3 () (5 () (7 () (9 () (11 () ()))))))
 ;; (1 3 5 7 9 11)
 ;; ((5 (1 () (3 () ())) (9 (7 () ()) (11 () ()))))
+
+;;  5
+;;  /\
+;; 1  9
+;; \  /\
+;; 3 7 11
+
+;; The process is tree recursive, which means it requires an exponential number
+;; of steps with respect to the depth of the tree. However, since we divide the
+;; problem in half at each step, the depth of the tree is logarithmic with
+;; respect to the number of nodes. The overall order of growth is proportional
+;; to e^(log n), or linear.
