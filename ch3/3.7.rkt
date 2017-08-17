dc49a0f4d4ee7d2bb4f8af3f052ea44c5e5f5b84
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-17 19:51:27 -0400

    3.7

---
+++ ch3/3.7.rkt
@@ -0,0 +1,48 @@
+#lang sicp
+
+(define (make-account balance initial-password)
+  (let* ((password-db (list initial-password))
+         (check-password (lambda (password)
+                           (letrec ((c (lambda (db)
+                                         (if (null? db)
+                                             #f
+                                             (or (eq? password (car db))
+                                                 (c (cdr db)))))))
+                             (c password-db))))
+         (add-password (lambda (new-password)
+                         (set! password-db
+                               (cons new-password password-db))))
+         (withdraw (lambda (n)
+                     (if (>= balance n)
+                         (begin (set! balance (- balance n))
+                                balance)
+                         "insufficient funds")))
+         (deposit (lambda (n)
+                    (set! balance (+ balance n))))
+         (dispatch (lambda (password m)
+                     (if (not (check-password password))
+                         (error "incorrect password")
+                         (cond ((eq? m 'add-password) add-password)
+                               ((eq? m 'withdraw) withdraw)
+                               ((eq? m 'deposit) deposit)
+                               (else (error "unknown request")))))))
+    (if (not (symbol? initial-password))
+        (error "invalid password")
+        dispatch)))
+
+(define (make-joint account password new-password)
+  ((account password 'add-password) new-password)
+  account)
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(let* ((peter-acc (make-account 100 'open-sesame))
+       (paul-acc (make-joint peter-acc 'open-sesame 'rosebud)))
+  (displayln ((paul-acc 'rosebud 'withdraw) 50))
+  (displayln ((paul-acc 'rosebud 'withdraw) 50))
+  (displayln ((paul-acc 'rosebud 'withdraw) 50)))
+;; 50
+;; 0
+;; insufficient funds
