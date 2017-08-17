df377965833fb162456a1f7c4d02974e34ea8b50
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-17 15:32:43 -0400

    3.6

---
+++ ch3/3.6.rkt
@@ -0,0 +1,35 @@
+#lang sicp
+
+(define rand-update
+  (let ((a #xfdeece66d)
+        (b #xb)
+        (m #x1000000000000))
+    (lambda (x)
+      (modulo (+ (* a x) b)
+              m))))
+
+(define rand
+  (let* ((x (runtime))
+         (generate (lambda ()
+                     (set! x (rand-update x))
+                     x))
+         (reset (lambda (y)
+                  (set! x y)))
+         (dispatch (lambda (m)
+                     (cond ((eq? m 'generate) (generate))
+                           ((eq? m 'reset) reset)
+                           (error "rand: unknown request:" m)))))
+    dispatch))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(displayln (rand 'generate))
+((rand 'reset) 1)
+(displayln (rand 'generate))
+((rand 'reset) 1)
+(displayln (rand 'generate))
+;; 60844757504805
+;; 68164576888
+;; 68164576888
