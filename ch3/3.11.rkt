e47f2e9d28dcad92fb4eaec552c3d6b2d6a34422
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-18 16:34:49 -0400

    3.11

---
+++ ch3/3.11.rkt
@@ -0,0 +1,44 @@
+#lang sicp
+
+(define (make-account balance)
+  (let* ((withdraw (lambda (n)
+                     (if (>= balance n)
+                         (begin (set! balance (- balance n))
+                                balance)
+                         "insufficient funds")))
+         (deposit (lambda (n)
+                    (set! balance (+ balance n))
+                    balance))
+         (dispatch (lambda (m)
+                     (cond ((eq? m 'withdraw) withdraw)
+                           ((eq? m 'deposit) deposit)
+                           (else (error "make-account: unknown request:" m))))))
+    dispatch))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(let ((acc (make-account 50)))
+  (displayln ((acc 'deposit) 40))
+  (displayln ((acc 'withdraw) 60)))
+;; 90
+;; 30
+
+;;             --------------
+;; global-env->|make-account|
+;;             -^------------
+;;             -|-----------
+;;         E1->|balance: 50|
+;;             |withdraw   |
+;;             |deposit    |
+;;             |dispatch   |<----|
+;;             -^-----------     |
+;;             -|----------     -|----------
+;;         E2->|amount: 40| E3->|amount: 60|
+;;             ------------     ------------
+;;         call to deposit  call to withdraw
+
+;; The local state for an account is kept in the first frame generated from the
+;; global environment. Only the implementation of the procedure make-account is
+;; shared between accounts.
