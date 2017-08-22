c8ea10c7187b9bb593d2733cbe86f07a463bcf85
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-22 14:51:23 -0400

    3.24

---
+++ ch3/3.24.rkt
@@ -0,0 +1,54 @@
+#lang sicp
+
+(define (make-table same-key?)
+  (let* ((table (list '<table>))
+         (assoc (lambda (key records)
+                  (letrec ((a (lambda (records)
+                                (cond ((null? records) #f)
+                                      ((same-key? key (caar records)) (car records))
+                                      (else (a (cdr records)))))))
+                    (a records))))
+         (lookup (lambda (key1 key2)
+                   (let ((subtable (assoc key1 (cdr table))))
+                     (if subtable
+                         (let ((record (assoc key2 (cdr subtable))))
+                           (if record
+                               (cdr record)
+                               #f))
+                         #f))))
+         (insert! (lambda (key1 key2 value)
+                   (let ((subtable (assoc key1 (cdr table))))
+                     (if subtable
+                         (let ((record (assoc key2 (cdr subtable))))
+                           (if record
+                               (set-cdr! record value)
+                               (set-cdr! subtable
+                                         (cons (cons key2 value)
+                                               (cdr subtable)))))
+                         (set-cdr! table
+                                   (cons (list key1
+                                               (cons key2 value))
+                                         (cdr table)))))))
+         (dispatch (lambda (m)
+                     (cond ((eq? m 'lookup) lookup)
+                           ((eq? m 'insert!) insert!)
+                           (else (error "make-table: undefined operation:" m))))))
+    dispatch))
+
+(define table (make-table equal?))
+
+(define get (table 'lookup))
+
+(define put (table 'insert!))
+
+(put 'row1 'col1 'apple)
+(put 'row1 'col2 'banana)
+(put 'row2 'col1 'orange)
+(get 'row1 'col1)
+(get 'row1 'col2)
+(get 'row2 'col1)
+(get 'row2 'col2)
+;; 'apple
+;; 'banana
+;; 'orange
+;; #f
