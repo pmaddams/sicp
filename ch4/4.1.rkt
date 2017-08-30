f2cfadb1141b4d7f44abaa2a50eeb7c8d69e9d8b
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-08-29 21:54:39 -0400

    4.1

--- ch4/4.1.rkt
+++ ch4/4.1.rkt
@@ -295,3 +295,28 @@
                                               (procedure-environment proc))))
           (else
            (error "apply: unknown procedure type:" proc)))))
+
+(define input-prompt ">>> ")
+
+(define (prompt-for-input string)
+  (display string)
+  (newline))
+
+(define (user-print object)
+  (if (compound-procedure? object)
+      (display (list 'compound-procedure
+                     (procedure-parameters object)
+                     (procedure-body object)
+                     '<procedure-env>))
+      (display object)))
+
+(define the-global-environment (setup-environment))
+
+(define (driver-loop)
+  (prompt-for-input input-prompt)
+  (let ((input (read)))
+    (let ((output (eval input the-global-environment)))
+      (user-print output)))
+  (driver-loop))
+
+(driver-loop)
