3323037397617c8d67b857e35bec425256fcdef3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-01 20:54:38 -0400

    1.10

--- ch1/1.10.rkt
+++ ch1/1.10.rkt
@@ -1,8 +1,8 @@
 #lang sicp
 
 (define (A x y)
-  (cond ((= y 0) 0)
-        ((= x 0) (* 2 y))
+  (cond ((zero? y) 0)
+        ((zero? x) (* 2 y))
         ((= y 1) 2)
         (else (A (dec x)
                  (A x (dec y))))))
@@ -21,20 +21,29 @@
 
 ;; (f n)
 ;; (A 0 n)
-;; (cond ((= n 0) 0)
-;;       ((= 0 0) (* 2 n))
+;; (cond ((zero? n) 0)
+;;       ((zero? 0) (* 2 n))
 ;;       ((= n 1) 2)
 ;;       (else (A (dec 0)
 ;;                (A 0 (dec n)))))
 ;; (* 2 n)
+
+(f 1)
+;; 2
+
+(f 2)
+;; 4
+
+(f 3)
+;; 6
 
 (define (g n)
   (A 1 n))
 
 ;; (g n)
 ;; (A 1 n)
-;; (cond ((= n 0) 0)
-;;       ((= 1 0) (* 2 n))
+;; (cond ((zero? n) 0)
+;;       ((zero? 1) (* 2 n))
 ;;       ((= n 1) 2)
 ;;       (else (A (dec 1)
 ;;                (A 1 (dec n)))))
@@ -42,16 +51,34 @@
 ;; (* 2 (A 1 (dec n)))
 ;; (expt 2 n)
 
+(g 1)
+;; 2
+
+(g 2)
+;; 4
+
+(g 3)
+;; 8
+
 (define (h n)
   (A 2 n))
 
 ;; (h n)
 ;; (A 2 n)
-;; (cond ((= n 0) 0)
-;;       ((= 2 0) (* 2 n))
+;; (cond ((zero? n) 0)
+;;       ((zero? 2) (* 2 n))
 ;;       ((= n 1) 2)
 ;;       (else (A (dec 2)
 ;;                (A 2 (dec n)))))
 ;; (A 1 (A 2 (dec n)))
 ;; (expt 2 (A 2 (dec n)))
 ;; (expt 2 (expt 2 ... )))
+
+(h 1)
+;; 2
+
+(h 2)
+;; 4
+
+(h 3)
+;; 16
