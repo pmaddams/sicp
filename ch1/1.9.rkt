51c069591e6c9151b8362b62bff8d75f9604d281
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-01 20:47:29 -0400

    1.9

--- ch1/1.9.rkt
+++ ch1/1.9.rkt
@@ -1,29 +1,29 @@
 #lang sicp
 
-(let ((+ (lambda (a b)
-           (if (= a 0)
-               b
-               (inc (+ (dec a) b))))))
+(letrec ((+ (lambda (a b)
+              (if (zero? a)
+                  b
+                  (inc (+ (dec a) b))))))
   (+ 4 5))
 
 ;; (+ 4 5)
-;; (if (= 4 0)
+;; (if (zero? 4)
 ;;     5
 ;;     (inc (+ (dec 4) 5)))
 ;; (inc (+ 3 5))
-;; (inc (if (= 3 0)
+;; (inc (if (zero? 3)
 ;;          5
 ;;          (inc (+ (dec 3) 5))))
 ;; (inc (inc (+ 2 5)))
-;; (inc (inc (if (= 2 0)
+;; (inc (inc (if (zero? 2)
 ;;               5
 ;;               (inc (+ (dec 2) 5)))))
 ;; (inc (inc (inc (+ 1 5))))
-;; (inc (inc (inc (if (= 1 0)
+;; (inc (inc (inc (if (zero? 1)
 ;;                    5
 ;;                    (inc (+ (dec 1) 5))))))
 ;; (inc (inc (inc (inc (+ 0 5)))))
-;; (inc (inc (inc (inc (if (= 0 0)
+;; (inc (inc (inc (inc (if (zero? 0)
 ;;                         5
 ;;                         (inc (+ (dec 0) 5)))))))
 ;; (inc (inc (inc (inc 5))))
@@ -34,30 +34,30 @@
 
 ;; The evolution of the process is recursive.
 
-(let ((+ (lambda (a b)
-           (if (= a 0)
-               b
-               (+ (dec a) (inc b))))))
+(letrec ((+ (lambda (a b)
+              (if (zero? a)
+                  b
+                  (+ (dec a) (inc b))))))
   (+ 4 5))
 
 ;; (+ 4 5)
-;; (if (= 4 0)
+;; (if (zero? 4)
 ;;     5
 ;;     (+ (dec 4) (inc 5)))
 ;; (+ 3 6)
-;; (if (= 3 0)
+;; (if (zero? 3)
 ;;     6
 ;;     (+ (dec 3) (inc 6)))
 ;; (+ 2 7)
-;; (if (= 2 0)
+;; (if (zero? 2)
 ;;     7
 ;;     (+ (dec 2) (inc 7)))
 ;; (+ 1 8)
-;; (if (= 1 0)
+;; (if (zero? 1)
 ;;     8
 ;;     (+ (dec 1) (inc 8)))
 ;; (+ 0 9)
-;; (if (= 0 0)
+;; (if (zero? 0)
 ;;     9
 ;;     (+ (dec 0) (inc 9)))
 ;; 9
