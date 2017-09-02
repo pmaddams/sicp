a896ffda49be947beff18606f5d01c0035082257
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-01 20:18:38 -0400

    1.8

--- ch1/1.8.rkt
+++ ch1/1.8.rkt
@@ -1,17 +1,20 @@
 #lang sicp
 
 (define (cbrt x)
-  (letrec ((improve (lambda (guess)
-                      (/ (+ (/ x
-                               (expt guess 2))
-                            (* guess 2))
-                         3)))
-           (good-enough? (lambda (prev guess)
-                           (< (abs (/ (- prev guess)
-                                      guess))
-                              0.001)))
-           (cbrt-iter (lambda (prev guess)
-                        (if (good-enough? prev guess)
-                            guess
-                            (cbrt-iter guess (improve guess))))))
-    (cbrt-iter 0.0 1.0)))
+  (let* ((square (lambda (x)
+                   (expt x 2)))
+         (improve (lambda (guess)
+                    (/ (+ (/ x (square guess))
+                          (* guess 2))
+                       3)))
+         (good-enough? (lambda (prev guess)
+                         (< (abs (/ (- prev guess) guess))
+                            0.001))))
+    (letrec ((c (lambda (prev guess)
+                  (if (good-enough? prev guess)
+                      guess
+                      (c guess (improve guess))))))
+      (c 0.0 1.0))))
+
+(cbrt 8)
+;; 2.000000000012062
