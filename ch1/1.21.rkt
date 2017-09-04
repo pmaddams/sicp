c82f2ad270c36fbae9e255a03d657937fb402d24
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-04 14:56:48 -0400

    '1.21'

--- ch1/1.21.rkt
+++ ch1/1.21.rkt
@@ -1,15 +1,15 @@
 #lang sicp
 
 (define (smallest-divisor n)
-  (letrec ((square (lambda (x)
-                     (expt x 2)))
-           (divides? (lambda (a b)
-                       (zero? (remainder b a))))
-           (find-divisor (lambda (test-divisor)
-                           (cond ((> (square test-divisor) n) n)
-                                 ((divides? test-divisor n) test-divisor)
-                                 (else (find-divisor (inc test-divisor)))))))
-    (find-divisor 2)))
+  (let ((square (lambda (x)
+                  (expt x 2)))
+        (divides? (lambda (a b)
+                    (zero? (remainder b a)))))
+    (letrec ((s (lambda (i)
+                  (cond ((> (square i) n) n)
+                        ((divides? i n) i)
+                        (else (s (inc i)))))))
+      (s 2))))
 
 (smallest-divisor 199)
 ;; 199
