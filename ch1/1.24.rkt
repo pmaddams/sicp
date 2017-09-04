981ccec0e5d5a8459bdef30dccd5e8558b5fda9a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-04 19:43:24 -0400

    1.24

--- ch1/1.24.rkt
+++ ch1/1.24.rkt
@@ -1,71 +1,67 @@
 #lang sicp
 
-(define (expmod base exp m)
-  (letrec ((square (lambda (x)
-                     (expt x 2)))
-           (e (lambda (exp)
-                (cond ((zero? exp) 1)
-                      ((even? exp) (remainder (square (e (/ exp 2)))
-                                              m))
-                      (else (remainder (* base (e (dec exp)))
-                                       m))))))
-    (e exp)))
+(define (expmod a n m)
+  (let ((square (lambda (x)
+                  (expt x 2))))
+    (letrec ((e (lambda (n)
+                  (cond ((zero? n) 1)
+                        ((even? n) (remainder (square (e (/ n 2))) m))
+                        (else (remainder (* a (e (dec n))) m))))))
+      (e n))))
 
-(define (fast-prime? n times)
-  (letrec ((fermat-test (lambda (n)
-                          (let ((a (inc (random (dec n)))))
-                            (= a (expmod a n n)))))
-           (f (lambda (times)
-                (cond ((zero? times) #t)
-                      ((fermat-test n) (f (dec times)))
-                      (else #f)))))
-    (f times)))
+(define (fermat-test n)
+  (let ((a (inc (random (dec n)))))
+    (= a (expmod a n n))))
 
 (define (prime? n)
-  (cond ((<= n 1) #f)
-        ((= n 2) #t)
-        ((even? n) #f)
-        (else (fast-prime? n 10))))
+  (letrec ((p (lambda (times)
+                (or (zero? times)
+                    (and (fermat-test n)
+                         (p (dec times)))))))
+    (and (> n 1)
+         (not (= n 2))
+         (odd? n)
+         (p 10))))
 
 (define (timed-prime-test n)
-  (let* ((report-prime (lambda (elapsed-time)
-                         (display " *** ")
-                         (display elapsed-time)))
-         (start-prime-test (lambda (start-time)
-                             (if (prime? n)
-                                 (report-prime (- (runtime) start-time))))))
-    (newline)
-    (display n)
-    (start-prime-test (runtime))))
+  (let ((start (runtime)))
+    (if (prime? n)
+        (let ((elapsed (- (runtime) start)))
+          (display n)
+          (display " *** ")
+          (display elapsed)
+          (newline)
+          elapsed))))
 
-(define (search-for-primes n bound)
-  (if (< n bound)
-      (if (odd? n)
-          (begin (timed-prime-test n)
-                 (search-for-primes (+ n 2) bound))
-          (search-for-primes (inc n) bound))
-      (newline)))
+(define (search-for-primes n lim)
+  (letrec ((s (lambda (n)
+                (if (< n lim)
+                    (begin (timed-prime-test n)
+                           (s (+ n 2)))))))
+    (if (even? n)
+        (set! n (inc n)))
+    (s n)))
 
 (search-for-primes 1000 1020)
-;; 1009 *** 197
-;; 1013 *** 200
-;; 1019 *** 208
+;; 1009 *** 205
+;; 1013 *** 213
+;; 1019 *** 224
 
 (search-for-primes 10000 10040)
-;; 10007 *** 246
-;; 10009 *** 240
-;; 10037 *** 244
+;; 10007 *** 267
+;; 10009 *** 292
+;; 10037 *** 263
 
 (search-for-primes 100000 100050)
-;; 100003 *** 281
-;; 100019 *** 285
-;; 100043 *** 289
+;; 100003 *** 298
+;; 100019 *** 306
+;; 100043 *** 302
 
 (search-for-primes 1000000 1000040)
-;; 1000003 *** 322
-;; 1000033 *** 320
-;; 1000037 *** 330
+;; 1000003 *** 358
+;; 1000033 *** 342
+;; 1000037 *** 351
 
-;; Since the Fermat test has theta of log(n) growth, the runtime should increase
-;; linearly as n increases by orders of magnitude. The data supports this
-;; hypothesis.
+;; Since the Fermat test has theta(log(n)) growth, the running time should
+;; increase linearly as n increases by orders of magnitude. The data supports
+;; this hypothesis.
