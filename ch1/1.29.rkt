c3067a7041c4e052db500b0a3e0bd7c74540aa8f
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-05 21:42:51 -0400

    1.29

--- ch1/1.29.rkt
+++ ch1/1.29.rkt
@@ -1,22 +1,22 @@
 #lang sicp
 
-(define (cube x) (expt x 3))
-
 (define (sum term a next b)
-  (letrec ((s (lambda (a)
+  (letrec ((s (lambda (a result)
                 (if (> a b)
-                    0
-                    (+ (term a)
-                       (s (next a)))))))
-    (s a)))
+                    result
+                    (s (next a)
+                       (+ result (term a)))))))
+    (s a 0)))
 
 (define (integral f a b dx)
-  (let ((add-dx (lambda (x)
-                  (+ x dx))))
-    (* (sum f (+ a (/ dx 2.0)) add-dx b)
-       dx)))
+  (* (sum f
+          (+ a (/ dx 2.0))
+          (lambda (x)
+            (+ x dx))
+          b)
+     dx))
 
-(define (simpson-integral f a b n)
+(define (simpson f a b n)
   (let* ((h (/ (- b a) n))
          (move (lambda (steps plus-or-minus)
                  (lambda (x)
@@ -27,14 +27,17 @@
           (* 2 (sum f ((move 1 +) a) (move 2 +) ((move 1 -) b)))
           (* 4 (sum f ((move 2 +) a) (move 2 +) ((move 2 -) b)))))))
 
+(define (cube x)
+  (expt x 3))
+
 (integral cube 0 1 0.01)
 ;; 0.24998750000000042
 
-(simpson-integral cube 0 1 100)
+(simpson cube 0 1 100)
 ;; 0.2467166666666667
 
 (integral cube 0 1 0.001)
 ;; 0.249999875000001
 
-(simpson-integral cube 0 1 1000)
+(simpson cube 0 1 1000)
 ;; 0.24966716666666663
