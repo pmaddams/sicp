2d918503218894346a3ab63efb02cd7d31b8d2df
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-07 20:14:35 -0400

    1.46

--- ch1/1.46.rkt
+++ ch1/1.46.rkt
@@ -1,32 +1,33 @@
 #lang sicp
 
+(define (good-enough? guess next)
+  (let ((tolerance 0.00001))
+    (< (abs (/ (- guess next) next))
+       tolerance)))
+
 (define (iterative-improve good-enough? improve)
   (lambda (guess)
     (letrec ((i (lambda (guess)
                   (let ((next (improve guess)))
                     (if (good-enough? guess next)
-                        guess
+                        next
                         (i next))))))
       (i guess))))
-
-(define (good-enough? guess next)
-  (let ((tolerance 0.00001))
-    (< (abs (/ (- guess next) next)) tolerance)))
 
 (define (average . args)
   (/ (apply + args)
      (length args)))
 
-(define (sqrt-ii x)
+(define (sqrt x)
   (let ((improve (lambda (guess)
                    (average guess (/ x guess)))))
     ((iterative-improve good-enough? improve) 1.0)))
 
-(sqrt-ii (expt 1000000 2))
-;; 1000000.103461242
+(sqrt (expt 1000000 2))
+;; 1000000.0000000054
 
 (define (fixed-point f guess)
   ((iterative-improve good-enough? f) guess))
 
 (fixed-point cos 1.0)
-;; 0.7390893414033927
+;; 0.7390822985224024
