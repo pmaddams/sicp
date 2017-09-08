03a781f8b15ddd4118572b8d751ce1b2cf99dcc6
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-07 20:06:21 -0400

    1.45

--- ch1/1.45.rkt
+++ ch1/1.45.rkt
@@ -1,42 +1,54 @@
 #lang sicp
 
 (define (fixed-point f guess)
-  (letrec ((tolerance 0.00001)
-           (close-enough? (lambda (a b)
-                            (< (abs (- a b)) tolerance)))
-           (try (lambda (guess)
-                  (let ((next (f guess)))
-                    (if (close-enough? guess next)
-                        next
-                        (try next))))))
-    (try guess)))
+  (let* ((tolerance 0.00001)
+         (close-enough? (lambda (a b)
+                          (< (abs (- a b)) tolerance))))
+    (letrec ((try (lambda (guess)
+                    (let ((next (f guess)))
+                      (if (close-enough? guess next)
+                          next
+                          (try next))))))
+      (try guess))))
+
+(define (average . args)
+  (/ (apply + args)
+     (length args)))
 
 (define (average-damp f)
-  (let ((average (lambda args
-                   (/ (apply + args)
-                      (length args)))))
-    (lambda (x)
-      (average x (f x)))))
+  (lambda (x)
+      (average x (f x))))
+
+(define (double f)
+  (lambda (x)
+    (f (f x))))
 
 (define (repeated f n)
-  (let ((double (lambda (f)
-                  (lambda (x)
-                    (f (f x))))))
-    (if (= n 1)
-        f
-        (repeated (double f) (dec n)))))
+  (if (= n 1)
+      f
+      (repeated (double f) (dec n))))
 
 (define (nth-root n)
   (lambda (x)
     (fixed-point ((repeated average-damp (ceiling (log n)))
-                  (lambda (y) (/ x (expt y (dec n)))))
+                  (lambda (y)
+                    (/ x (expt y (dec n)))))
                  1.0)))
 
-(do ((n 2 (inc n))
-     (lim 10))
-  ((> n lim))
-  (display ((nth-root n) (expt n n)))
+(define (displayln x)
+  (display x)
   (newline))
+
+(define (enumerate-interval low high)
+  (letrec ((e (lambda (i result)
+                (if (< i low)
+                    result
+                    (e (dec i) (cons i result))))))
+    (e high '())))
+
+(for-each displayln (map (lambda (n)
+                           ((nth-root n) (expt n n)))
+                         (enumerate-interval 2 10)))
 ;; 2.000000000000002
 ;; 3.000001464168659
 ;; 4.000000000000006
