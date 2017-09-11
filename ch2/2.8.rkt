96ecd35867324bb017ca8be2f231043499ae9055
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-10 20:07:05 -0400

    2.8

--- ch2/2.8.rkt
+++ ch2/2.8.rkt
@@ -1,8 +1,8 @@
 #lang sicp
 
-;; Let i - j = k, where i, j, and k are intervals. The lower bound of k is equal
-;; to the lower bound of i minus the upper bound of j, and the upper bound of k 
-;; is equal to the upper bound of i minus the lower bound of j.
+;; Let z = x - y, where x, y, and z are intervals. The lower bound of z is equal
+;; to the lower bound of x minus the upper bound of y, and the upper bound of z
+;; is equal to the upper bound of x minus the lower bound of y.
 
 (define make-interval cons)
 
@@ -31,7 +31,7 @@
                 (make-interval (/ 1.0 (upper-bound y))
                                (/ 1.0 (lower-bound y)))))
 
-(let ((i (make-interval 1 2))
-      (j (make-interval 3 4)))
-  (display (sub-interval i j)))
+(let ((x (make-interval 1 2))
+      (y (make-interval 3 4)))
+  (display (sub-interval x y)))
 ;; (-3 . -1)
