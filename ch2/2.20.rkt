2f7a68ffdeaac030f55db762f7aed728d3d3a2fd
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-12 19:47:19 -0400

    2.20

--- ch2/2.20.rkt
+++ ch2/2.20.rkt
@@ -1,12 +1,28 @@
 #lang sicp
 
 (define (same-parity first . rest)
-  (letrec ((check-parity (if (odd? first)
-                             odd?
-                             even?))
-           (build-list (lambda (rest)
-                         (cond ((null? rest) '())
-                               ((check-parity (car rest))
-                                (cons (car rest) (build-list (cdr rest))))
-                               (else (build-list (cdr rest)))))))
-    (cons first (build-list rest))))
+  (let ((same? (if (odd? first)
+                   odd?
+                   even?)))
+    (letrec ((s (lambda (rest result)
+                  (cond ((null? rest)
+                         result)
+                        ((same? (car rest))
+                         (s (cdr rest)
+                            (append result (list (car rest)))))
+                        (else
+                         (s (cdr rest)
+                            result))))))
+      (cons first (s rest '())))))
+
+(define (displayln x)
+  (display x)
+  (newline))
+
+(for-each displayln
+          (map (lambda (x)
+                 (apply same-parity x))
+               '((1 2 3 4 5 6 7)
+                 (2 3 4 5 6 7))))
+;; (1 3 5 7)
+;; (2 4 6)
