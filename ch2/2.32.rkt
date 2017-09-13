556520554c5f85671a5ce4facddbffdd7f4a9bd0
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-13 14:39:44 -0400

    2.32

--- ch2/2.32.rkt
+++ ch2/2.32.rkt
@@ -3,16 +3,14 @@
 (define (subsets s)
   (if (null? s)
       '(())
-      (let ((rest (subsets (cdr s)))
-            (p (lambda (x)
-                 (cons (car s) x))))
-        (append rest (map p rest)))))
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x)
+                            (cons (car s) x))
+                          rest)))))
 
-;; The base case for this procedure results from taking the empty list as an
-;; argument, which happens once we have traversed the list to the end. In this
-;; case we return the empty set. Traveling up the stack of recursive procedures,
-;; we now have a list of one element. The procedure which we map to rest
-;; consists of attaching this element to all the elements in rest, and appending
-;; the results to become the new rest variable. As we continue traveling up the
-;; stack, this procedure retains the results gathered so far, and appends the
-;; new list of each successive element attached to those results.
+;; The base case occurs when we have traversed the list to the end, and returns
+;; the empty set. What we want is to keep this, and attach the mapped list of
+;; the preceding element consed to each subset so far.
+
+(display (subsets '(1 2 3)))
+;; (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
