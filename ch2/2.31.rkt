ec31a447ebe0409870704fb263c8ce18cd7764a9
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-13 14:30:00 -0400

    2.31

--- ch2/2.31.rkt
+++ ch2/2.31.rkt
@@ -1,14 +1,15 @@
 #lang sicp
 
-(define (tree-map p t)
-  (letrec ((pt (lambda (st)
+(define (tree-map proc t)
+  (letrec ((tm (lambda (st)
                  (if (not (pair? st))
-                     (p st)
-                     (map pt st)))))
-    (pt t)))
+                     (proc st)
+                     (map tm st)))))
+    (tm t)))
 
-(let ((square (lambda (x)
-                (expt x 2)))
-      (t '(1 (2 (3 4) 5) (6 7))))
+(define (square x)
+  (expt x 2))
+
+(let ((t '(1 (2 (3 4) 5) (6 7))))
   (display (tree-map square t)))
 ;; (1 (4 (9 16) 25) (36 49))
