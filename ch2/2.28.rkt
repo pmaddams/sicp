6f25b51adbb8792308394edf0aa86d6635bd6d85
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-13 11:58:58 -0400

    2.28

--- ch2/2.28.rkt
+++ ch2/2.28.rkt
@@ -1,19 +1,23 @@
 #lang sicp
 
 (define (fringe t)
-  (letrec ((f (lambda (t l)
-                (cond ((null? t) l)
-                      ((list? (car t)) (append (f (car t) '())
-                                               (f (cdr t) l)))
-                      (else (cons (car t) (f (cdr t) l)))))))
+  (letrec ((f (lambda (t result)
+                (cond ((null? t)
+                       result)
+                      ((list? (car t))
+                       (append (f (car t) '())
+                               (f (cdr t) result)))
+                      (else
+                       (cons (car t)
+                             (f (cdr t) result)))))))
     (f t '())))
 
 (define (displayln x)
   (begin (display x)
          (newline)))
 
-(let ((x '((1 2) (3 4))))
-  (displayln (fringe x))
-  (displayln (fringe (list x x))))
+(let ((t '((1 2) (3 4))))
+  (displayln (fringe t))
+  (displayln (fringe (list t t))))
 ;; (1 2 3 4)
 ;; (1 2 3 4 1 2 3 4)
