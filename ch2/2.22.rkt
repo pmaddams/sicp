d6dc9e4924fe03bbf13f7d5bb4baa7ec6b6cef08
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-12 20:40:11 -0400

    2.22

--- ch2/2.22.rkt
+++ ch2/2.22.rkt
@@ -3,14 +3,14 @@
 (define (square x)
   (expt x 2))
 
-(define (square-list-a items)
-  (letrec ((iter (lambda (things answer)
-                   (if (null? things)
-                       answer
-                       (iter (cdr things)
-                             (cons (square (car things))
-                                   answer))))))
-    (iter items '())))
+(define (square-list-a l)
+  (letrec ((s (lambda (l result)
+                (if (null? l)
+                    result
+                    (s (cdr l)
+                       (cons (square (car l))
+                             result))))))
+    (s l '())))
 
 (define (displayln x)
   (display x)
@@ -22,14 +22,14 @@
 ;; This procedure conses each squared item in the list to the front of the list
 ;; so far, so the list ends up in reverse order.
 
-(define (square-list-b items)
-  (letrec ((iter (lambda (things answer)
-                   (if (null? things)
-                       answer
-                       (iter (cdr things)
-                             (cons answer
-                                   (square (car things))))))))
-    (iter items '())))
+(define (square-list-b l)
+  (letrec ((s (lambda (l result)
+                (if (null? l)
+                    result
+                    (s (cdr l)
+                       (cons result
+                             (square (car l))))))))
+    (s l '())))
 
 (displayln (square-list-b '(1 2 3 4)))
 ;; ((((() . 1) . 4) . 9) . 16)
@@ -37,14 +37,14 @@
 ;; In this case, we are consing new values to the end of the list so far,
 ;; producing an invalid list.
 
-(define (square-list-c items)
-  (letrec ((iter (lambda (things answer)
-                   (if (null? things)
-                       answer
-                       (iter (cdr things)
-                             (append answer
-                                     (list (square (car things)))))))))
-    (iter items '())))
+(define (square-list-c l)
+  (letrec ((s (lambda (l result)
+                (if (null? l)
+                    result
+                    (s (cdr l)
+                       (append result
+                               (list (square (car l)))))))))
+    (s l '())))
 
 (displayln (square-list-c '(1 2 3 4)))
 ;; (1 4 9 16)
