76da510489dfe6b3f58df761fadc5cb39e633a72
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-13 20:55:14 -0400

    2.38

--- ch2/2.38.rkt
+++ ch2/2.38.rkt
@@ -1,46 +1,36 @@
 #lang sicp
 
-(define (fold-right op init seq)
+(define (foldr proc init seq)
   (letrec ((f (lambda (seq)
                 (if (null? seq)
                     init
-                    (op (car seq) (f (cdr seq)))))))
+                    (proc (car seq)
+                          (f (cdr seq)))))))
     (f seq)))
 
-(define (fold-left op init seq)
-  (letrec ((f (lambda (result rest)
-                (if (null? rest)
+(define (foldl proc init seq)
+  (letrec ((f (lambda (seq result)
+                (if (null? seq)
                     result
-                    (f (op result (car rest))
-                       (cdr rest))))))
-    (f init seq)))
+                    (f (cdr seq)
+                       (proc (car seq)
+                             result))))))
+    (f seq init)))
 
 (define (displayln x)
   (display x)
   (newline))
 
-(displayln (fold-right / 1 '(1 2 3)))
+(let ((l '(1 2 3)))
+  (displayln (foldr / 1 l))
+  (displayln (foldl / 1 l))
+  (displayln (foldr list '() l))
+  (displayln (foldl list '() l)))
 ;; 3/2
-
-(displayln (fold-left / 1 '(1 2 3)))
-;; 1/6
-
-(displayln (fold-right list '() '(1 2 3)))
+;; 3/2
 ;; (1 (2 (3 ())))
+;; (3 (2 (1 ())))
 
-(displayln (fold-left list '() '(1 2 3)))
-;; (((() 1) 2) 3)
-
-;; For fold-right and fold-left to yield the same result, the commutative
-;; property must hold for the operator, which is to say that if the left and
-;; right operands are switched, the result of an operation will be the same.
-
-(displayln (fold-right + 0 '(1 2 3)))
-(displayln (fold-left + 0 '(1 2 3)))
-;; 6
-;; 6
-
-(displayln (fold-right * 1 '(1 2 3)))
-(displayln (fold-left * 1 '(1 2 3)))
-;; 6
-;; 6
+;; For foldr and foldl to yield the same result, the commutative property must
+;; hold for the operator, which is to say that if the left and right operands
+;; are switched, the result of an operation will be the same.
