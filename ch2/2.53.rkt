ceac8a88ec2a54287f42b1ba6756c7e55f2686af
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-15 20:28:58 -0400

    2.53

--- ch2/2.53.rkt
+++ ch2/2.53.rkt
@@ -1,33 +1,21 @@
 #lang sicp
 
-(define (memq item x)
-  (letrec ((m (lambda (x)
-                (cond ((null? x) #f)
-                      ((eq? item (car x)) x)
-                      (else (m (cdr x)))))))
-    (m x)))
-
 (define (displayln x)
   (display x)
   (newline))
 
-(displayln (list 'a 'b 'c))
+(for-each displayln
+          (list (list 'a 'b 'c)
+                (list (list 'george))
+                (cdr '((x1 x2) (y1 y2)))
+                (cadr '((x1 x2) (y1 y2)))
+                (pair? (car '(a short list)))
+                (memq 'red '((red shoes) (blue socks)))
+                (memq 'red '(red shoes blue socks))))
 ;; (a b c)
-
-(displayln (list (list 'george)))
 ;; ((george))
-
-(displayln (cdr '((x1 x2) (y1 y2))))
 ;; ((y1 y2))
-
-(displayln (cadr '((x1 x2) (y1 y2))))
 ;; (y1 y2)
-
-(displayln (pair? (car '(a short list))))
 ;; #f
-
-(displayln (memq 'red '((red shoes) (blue socks))))
 ;; #f
-
-(displayln (memq 'red '(red shoes blue socks)))
 ;; (red shoes blue socks)
