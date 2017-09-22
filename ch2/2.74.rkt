f7e44366d1025f65895b4b7f4350d7d8365c58aa
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-21 21:02:44 -0400

    2.74

--- ch2/2.74.rkt
+++ ch2/2.74.rkt
@@ -29,146 +29,107 @@
 
 (define put (table 'put))
 
-(define (make-personnel-file-a)
-  (let* ((all-records '())
-         (record car)
-         (left-branch cadr)
-         (right-branch caddr)
-         (make-record (lambda (given-employee given-address given-salary)
-                        (list given-employee given-address given-salary)))
-         (employee car)
-         (address cadr)
-         (salary caddr)
-         (make-tree (lambda (given-record left right)
-                      (list given-record left right)))
-         (get-record
-          (lambda (given-employee-sym)
-            (letrec ((given-employee (symbol->string given-employee-sym))
-                     (l (lambda (set-of-records)
-                          (if (null? set-of-records)
-                              #f
-                              (let* ((this-record (record set-of-records))
-                                     (this-employee (symbol->string
-                                                     (employee this-record))))
-                                (cond ((string<? given-employee this-employee)
-                                       (l (left-branch set-of-records)))
-                                      ((string>? given-employee this-employee)
-                                       (l (right-branch set-of-records)))
-                                      ((string=? given-employee this-employee)
-                                       this-record)
-                                      (else
-                                       #f)))))))
-              (l all-records))))
-         (add-record
-          (lambda (given-record)
-            (letrec ((given-employee (symbol->string (employee given-record)))
-                     (a (lambda (set-of-records)
-                          (if (null? set-of-records)
-                              (make-tree given-record '() '())
-                              (let ((this-employee (symbol->string
-                                                    (employee (record set-of-records)))))
-                                (cond ((string<? given-employee this-employee)
-                                       (make-tree (record set-of-records)
-                                                  (a (left-branch set-of-records))
-                                                  (right-branch set-of-records)))
-                                      ((string>? given-employee this-employee)
-                                       (make-tree (record set-of-records)
-                                                  (left-branch set-of-records)
-                                                  (a (right-branch set-of-records))))
-                                      (else set-of-records)))))))
-              (set! all-records (a all-records)))))
-         (dispatch (lambda (m)
-                     (cond ((eq? m 'add-record)
-                            (lambda (given-employee given-address given-salary)
-                              (add-record
-                               (make-record given-employee given-address given-salary))))
-                           ((eq? m 'get-record)
-                            (lambda (given-employee)
-                              (let ((this-record (get-record given-employee)))
-                                (if (not this-record)
-                                    #f
-                                    (let* ((this-address (address this-record))
-                                           (this-salary (salary this-record)))
-                                      (list given-employee this-address this-salary))))))
-                           (else
-                            (error "personnel-file-a: undefined operation:" m))))))
-    dispatch))
+(define attach cons)
 
-(define (make-personnel-file-b)
-  (let* ((all-records '())
-         (record car)
-         (make-record (lambda (given-employee given-address given-salary)
-                        (list given-employee given-address given-salary)))
-         (employee car)
-         (address cadr)
-         (salary caddr)
-         (get-record (lambda (given-employee-sym)
-                       (letrec ((given-employee (symbol->string given-employee-sym))
-                                (l (lambda (set-of-records)
-                                     (if (null? set-of-records)
-                                         #f
-                                         (let* ((this-record (record set-of-records))
-                                                (this-employee (symbol->string
-                                                                (employee this-record))))
-                                           (if (string=? given-employee this-employee)
-                                               this-record
-                                               (l (cdr set-of-records))))))))
-                         (l all-records))))
-         (add-record (lambda (given-record)
-                       (set! all-records (cons given-record all-records))))
-         (dispatch (lambda (m)
-                     (cond ((eq? m 'add-record)
-                            (lambda (given-employee given-address given-salary)
-                              (add-record
-                               (make-record given-employee given-address given-salary))))
-                           ((eq? m 'get-record)
-                            (lambda (given-employee)
-                              (let ((this-record (get-record given-employee)))
-                                (if (not this-record)
-                                    #f
-                                    (let* ((this-address (address this-record))
-                                           (this-salary (salary this-record)))
-                                      (list given-employee this-address this-salary))))))
-                           (else (error "personnel-file-b: undefined operation:" m))))))
-    dispatch))
+(define (type obj)
+  (if (pair? obj)
+      (car obj)
+      (error "type: invalid object:" obj)))
 
-(define personnel-file-a (make-personnel-file-a))
+(define (value obj)
+  (if (pair? obj)
+      (cdr obj)
+      (error "value: invalid object:" obj)))
 
-(define personnel-file-b (make-personnel-file-b))
+(define (apply-generic op . args)
+  (let* ((types (map type args))
+         (proc (get op types)))
+    (if proc
+        (apply proc (map value args))
+        (error "apply-generic: no method for these types:" (list op types)))))
 
-(put 'personnel-file-a 'get-record (personnel-file-a 'get-record))
+(define (get-record dept employee)
+  (let ((r ((get 'record dept) employee)))
+    (if (value r)
+        (list dept
+              employee
+              (apply-generic 'address r)
+              (apply-generic 'salary r))
+        #f)))
 
-(put 'personnel-file-a 'add-record (personnel-file-a 'add-record))
+(define department car)
 
-(put 'personnel-file-b 'get-record (personnel-file-b 'get-record))
+(define name cadr)
 
-(put 'personnel-file-b 'add-record (personnel-file-b 'add-record))
+(define address caddr)
 
-(define (get-record personnel-file employee)
-  ((get personnel-file 'get-record) employee))
+(define salary cadddr)
 
-(define (add-record personnel-file employee address salary)
-  ((get personnel-file 'add-record) employee address salary))
-
-(define (get-salary personnel-file employee)
-  (caddr (get-record personnel-file employee)))
+(define (get-salary dept employee)
+  (let ((r (get-record dept employee)))
+    (if r
+        (salary r)
+        #f)))
 
 (define (find-employee-record employee)
-  (let ((all-personnel-files (list
-                              'personnel-file-a
-                              'personnel-file-b)))
-    (letrec ((f (lambda (personnel-files)
-                  (if (null? personnel-files)
-                      #f
-                      (let* ((personnel-file (car personnel-files))
-                             (employee-record (get-record personnel-file employee)))
-                        (if employee-record
-                            employee-record
-                            (f (cdr personnel-files))))))))
-      (f all-personnel-files))))
+  (let ((depts '(dept-a dept-b)))
+    (letrec ((f (lambda (depts)
+                  (and (not (null? depts))
+                       (or (get-record (car depts)
+                                       employee)
+                           (f (cdr depts)))))))
+      (f depts))))
 
-;; A uniform record API should be implemented that uses a layer of "glue code"
-;; to present records in the same way to the interdepartmental system. This way,
-;; the representation of records is irrelevant. When a new company is acquired,
-;; this API service should be implemented on top of that company's existing
-;; database.
+(define (displayln x)
+  (display x)
+  (newline))
+
+(define (load-dept-a-records)
+  (let ((records '(("Harold" "32 Vassar St" 10000)
+                   ("Gerald" "32 Vassar St" 20000)))
+        (tag (lambda (x)
+               (attach 'dept-a x))))
+    (display "loading department A records...")
+    (put 'record 'dept-a
+         (lambda (employee)
+           (tag (assoc employee records))))
+    (put 'address '(dept-a) cadr)
+    (put 'salary '(dept-a) caddr)
+    (displayln "done.")))
+
+(define (load-dept-b-records)
+  (let ((records '(("Ken" 30000 "600 Mountain Ave")
+                   ("Dennis" 40000 "600 Mountain Ave")))
+        (tag (lambda (x)
+               (attach 'dept-b x))))
+    (display "loading department B records...")
+    (put 'record 'dept-b
+         (lambda (employee)
+           (tag (assoc employee records))))
+    (put 'salary '(dept-b) cadr)
+    (put 'address '(dept-b) caddr)
+    (displayln "done.")))
+
+(load-dept-a-records)
+;; loading department A records...done.
+
+(load-dept-b-records)
+;; loading department B records...done.
+
+(for-each (lambda (employee)
+            (let ((r (find-employee-record employee)))
+              (if r
+                  (displayln (get-salary (department r)
+                                         employee)))))
+          (list "Harold"
+                "Gerald"
+                "Ken"
+                "Dennis"))
+;; 10000
+;; 20000
+;; 30000
+;; 40000
+
+;; When Insatiable takes over a new company, their information systems must tag
+;; provided records with a unique identifier for that department, and provide
+;; public, department-specific accessors for information within each record.
