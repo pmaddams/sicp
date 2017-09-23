583b0db22dedabc0e3a59a340b6ebe289d76e073
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-22 21:22:39 -0400

    3.1

--- ch3/3.1.rkt
+++ ch3/3.1.rkt
@@ -10,8 +10,24 @@
   (display x)
   (newline))
 
-(let ((A (make-accumulator 5)))
-  (displayln (A 10))
-  (displayln (A 10)))
+(define (enumerate-interval low high)
+  (letrec ((e (lambda (i result)
+                (if (< i low)
+                    result
+                    (e (dec i)
+                       (cons i result))))))
+    (e high '())))
+
+(let ((A (make-accumulator 1)))
+  (for-each (lambda (n)
+              (displayln (A n)))
+            (enumerate-interval 2 10)))
+;; 3
+;; 6
+;; 10
 ;; 15
-;; 25
+;; 21
+;; 28
+;; 36
+;; 45
+;; 55
