9c56c47d938fb530c1c8d1cad1d7f11482e0e8a6
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-24 13:29:17 -0400

    3.7

--- ch3/3.7.rkt
+++ ch3/3.7.rkt
@@ -1,49 +1,55 @@
 #lang sicp
 
-(define (make-account balance initial-password)
-  (let* ((password-db (list initial-password))
-         (check-password (lambda (password)
-                           (letrec ((c (lambda (db)
-                                         (if (null? db)
-                                             #f
-                                             (or (eq? password (car db))
-                                                 (c (cdr db)))))))
-                             (c password-db))))
-         (add-password (lambda (new-password)
-                         (set! password-db
-                               (cons new-password password-db))))
+(define (make-account balance secret)
+  (let* ((db (list secret))
+         (authorized? (lambda (password)
+                        (member password db)))
+         (add (lambda (password)
+                (set! db
+                      (cons password db))))
+         (reject (lambda (_)
+                   "incorrect password"))
          (withdraw (lambda (n)
                      (if (>= balance n)
-                         (begin (set! balance (- balance n))
+                         (begin (set! balance
+                                      (- balance n))
                                 balance)
                          "insufficient funds")))
          (deposit (lambda (n)
-                    (set! balance (+ balance n))))
+                    (set! balance
+                          (+ balance n))
+                    balance))
          (dispatch (lambda (password m)
-                     (if (not (check-password password))
-                         (error "incorrect password")
+                     (if (not (authorized? password))
+                         reject
                          (case m
-                           ('add-password add-password)
+                           ('add add)
                            ('withdraw withdraw)
                            ('deposit deposit)
                            (else (error "account: unknown method:" m)))))))
-    (if (not (symbol? initial-password))
-        (error "invalid password")
-        dispatch)))
+    dispatch))
 
-(define (make-joint account password new-password)
-  ((account password 'add-password) new-password)
+(define (make-joint account old-password new-password)
+  ((account old-password 'add) new-password)
   account)
 
 (define (displayln x)
   (display x)
   (newline))
 
+(define (enumerate-interval low high)
+  (letrec ((e (lambda (i result)
+                (if (< i low)
+                    result
+                    (e (dec i)
+                       (cons i result))))))
+    (e high '())))
+
 (let* ((peter-acc (make-account 100 'open-sesame))
        (paul-acc (make-joint peter-acc 'open-sesame 'rosebud)))
-  (displayln ((paul-acc 'rosebud 'withdraw) 50))
-  (displayln ((paul-acc 'rosebud 'withdraw) 50))
-  (displayln ((paul-acc 'rosebud 'withdraw) 50)))
+  (for-each (lambda (_)
+              (displayln ((paul-acc 'rosebud 'withdraw) 50)))
+            (enumerate-interval 1 3)))
 ;; 50
 ;; 0
 ;; insufficient funds
