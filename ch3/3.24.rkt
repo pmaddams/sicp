b1fbd42a4e5601c4e8af8bb4757fe143fddded69
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-24 20:37:13 -0400

    3.24

--- ch3/3.24.rkt
+++ ch3/3.24.rkt
@@ -1,49 +1,46 @@
 #lang sicp
 
-(define (make-table same-key?)
+(define (make-table same?)
   (let* ((table (list '<table>))
-         (get (lambda (k1 k2)
-                   (let ((subtable (assoc k1 (cdr table))))
-                     (if subtable
-                         (let ((record (assoc k2 (cdr subtable))))
-                           (if record
-                               (cdr record)
-                               #f))
-                         #f))))
-         (put (lambda (k1 k2 v)
-                    (let ((subtable (assoc k1 (cdr table))))
-                      (if subtable
-                          (let ((record (assoc k2 (cdr subtable))))
-                            (if record
-                                (set-cdr! record v)
-                                (set-cdr! subtable
-                                          (cons (cons k2 v)
-                                                (cdr subtable)))))
-                          (set-cdr! table
-                                    (cons (list k1
-                                                (cons k2 v))
-                                          (cdr table)))))))
+         (assm (lambda (key records)
+                 (letrec ((a (lambda (records)
+                               (cond ((null? records)
+                                      #f)
+                                     ((same? key (caar records))
+                                      (car records))
+                                     (else
+                                      (a (cdr records)))))))
+                   (a records))))
+         (get (lambda (key)
+                (let ((record (assm key (cdr table))))
+                  (if record
+                      (cdr record)
+                      #f))))
+         (put (lambda (key value)
+                (let ((record (assm key (cdr table))))
+                  (if record
+                      (set-cdr! record value)
+                      (set-cdr! table
+                                (cons (cons key value)
+                                      (cdr table)))))))
          (dispatch (lambda (m)
                      (case m
                        ('get get)
                        ('put put)
-                       (else (error "table: unknown method:" m))))))
+                       (else "table: unknown method:" m)))))
     dispatch))
 
-(define table (make-table equal?))
+(define table (make-table eq?))
 
 (define get (table 'get))
 
 (define put (table 'put))
 
-(put 'a 'a 'apple)
-(put 'a 'b 'banana)
-(put 'b 'a 'orange)
-(get 'a 'a)
-(get 'a 'b)
-(get 'b 'a)
-(get 'b 'b)
+(put 'a 'apple)
+(put 'b 'banana)
+(get 'a)
+(get 'b)
+(get 'c)
 ;; 'apple
 ;; 'banana
-;; 'orange
 ;; #f
