a01c4130a32111c278146b7444dbf1b9a80db77a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2017-09-24 21:30:20 -0400

    3.25

--- ch3/3.25.rkt
+++ ch3/3.25.rkt
@@ -1,43 +1,50 @@
 #lang sicp
 
-(define (make-table same-key?)
-  (let* ((table (list '<table>))
-         (get (lambda (ks)
-                   (letrec ((g (lambda (ks current-table)
-                                  (if (null? ks)
-                                      #f
-                                      (let ((result (assoc (car ks)
-                                                           (cdr current-table))))
-                                        (if result
-                                            (if (null? (cdr ks))
-                                                (cdr result)
-                                                (g (cdr ks)
-                                                    result))
-                                            #f))))))
-                     (g ks table))))
-         (put (lambda (ks v)
-                    (letrec ((p (lambda (ks current-table)
-                                   (if (null? ks)
-                                       (error "put: empty list of keys")
-                                       (let ((result (assoc (car ks)
-                                                            (cdr current-table))))
-                                         (if result
-                                             (if (null? (cdr ks))
-                                                 (set-cdr! result v)
-                                                 (p (car ks)
-                                                     result))
-                                             (if (null? (cdr ks))
-                                                 (set-cdr! current-table
-                                                           (cons (cons (car ks)
-                                                                       v)
-                                                                 (cdr current-table)))
-                                                 (let ((new-table (list (car ks))))
-                                                   (set-cdr! current-table
-                                                             (cons new-table
-                                                                   (cdr current-table)))
-                                                   (p (car ks)
-                                                       new-table)))))))))
-                      (p ks table))))
+(define (make-table same?)
+  (let* ((table '((<table>)))
+         (assm (lambda (key records)
+                 (letrec ((a (lambda (records)
+                               (cond ((null? records)
+                                      #f)
+                                     ((same? key (caaar records))
+                                      (car records))
+                                     (else
+                                      (a (cdr records)))))))
+                   (a records))))
+         (get (lambda (keys)
+                (letrec ((g (lambda (keys table)
+                              (if (null? keys)
+                                  #f
+                                  (let ((found (assm (car keys)
+                                                     (cdr table))))
+                                    (if found
+                                        (if (null? (cdr keys))
+                                            (cdar found)
+                                            (g (cdr keys)
+                                               found))
+                                        #f))))))
+                  (g keys table))))
+         (put (lambda (keys value)
+                (letrec ((p (lambda (keys table)
+                              (let ((found (assm (car keys)
+                                                 (cdr table))))
+                                (if found
+                                    (if (null? (cdr keys))
+                                        (set-cdr! (car found) value)
+                                        (p (cdr keys)
+                                           found))
+                                    (if (null? (cdr keys))
+                                        (set-cdr! table
+                                                  (cons (list (cons (car keys)
+                                                                    value))
+                                                        (cdr table)))
+                                        (let ((new (list (list (car keys)))))
+                                          (set-cdr! table
+                                                    (cons new
+                                                          (cdr table)))
+                                          (p (cdr keys)
+                                             new))))))))
+                  (p keys table))))
          (dispatch (lambda (m)
                      (case m
                        ('get get)
@@ -45,8 +52,17 @@
                        (else (error "table: unknown method:" m))))))
     dispatch))
 
-(define table (make-table equal?))
+(define table (make-table eq?))
 
 (define get (table 'get))
 
 (define put (table 'put))
+
+(put '(a) 'apple)
+(put '(a b) 'banana)
+(get '(a))
+(get '(a b))
+(get '(a b c))
+;; 'apple
+;; 'banana
+;; #f
