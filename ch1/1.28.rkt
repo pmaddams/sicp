9d7c9126e0bf6a65677c7dcf188ce4fe0b9e062c
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-10-22 20:02:19 -0400

    prime

--- ch1/1.28.rkt
+++ ch1/1.28.rkt
@@ -1,36 +1,36 @@
 #lang racket/base
 
-(require racket/generator)
-
 (module+ test
   (require rackunit))
 
 (define (make-prime? f)
   (lambda (n)
-    (if (< n 2)
-        #f
-        (f n))))
+    (and (not (< n 2))
+         (or (<= n 3)
+             (f n)))))
 
 (define (trial-division n)
-  (define (loop i)
+  (let loop ((i 2))
     (cond ((= n i) #t)
           ((zero? (modulo n i)) #f)
-          (else (loop (add1 i)))))
-  (loop 2))
+          (else (loop (add1 i))))))
 
 (define (fermat k)
   (lambda (n)
-    ()))
+    (let loop ((k k))
+      (or (zero? k)
+          (let ((a (random 2 (sub1 n))))
+            (and (= 1 (expmod a (sub1 n) n))
+                 (loop (sub1 k))))))))
+
+(define (expmod b x m)
+  (let loop ((x x))
+    (cond ((zero? x) 1)
+          ((even? x) (modulo (square (loop (/ x 2))) m))
+          (else (modulo (* b (loop (sub1 x))) m)))))
 
 (define (square n)
   (* n n))
-
-(define (expmod b x m)
-  (define (loop b x)
-    (cond ((zero? x) 1)
-          ((even? x) (modulo (square (loop b (/ x 2))) m))
-          (else (modulo (* b (loop b (sub1 x))) m))))
-  (loop b x))
 
 (module+ test
   (for ((i (in-range 10)))
@@ -39,6 +39,25 @@
           (m (random 100)))
       (check = (expmod b x m) (modulo (expt b x) m)))))
 
-(define (miller-rabin k)
-  (lambda (n)
-    ()))
+#;(define (miller-expmod b x m)
+    (define (loop b x)
+      (cond ((zero? x) 1)
+            ((even? x) (let ((x (modulo (square (loop b (/ x 2))) m)))
+                         (and (not ())
+                              (not ())
+                              x)))
+            (else (modulo (* b (loop b (sub1 x))) m))))
+    (loop b x))
+
+#;(define (miller-rabin k)
+    (lambda (n)
+      (define (loop k)
+        (or (zero? k)
+            (let ((a (random 2 (sub1 n))))
+              ())))
+      (loop k)))
+
+(module+ test
+  (let ((prime? (make-prime? (fermat 10))))
+    (for ((n '(2 3 5 7 11 13)))
+      (check-true (prime? n)))))
