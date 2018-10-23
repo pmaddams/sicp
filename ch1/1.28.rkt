83b4ecd23de7fe2f14855e8b7d71c822149f41de
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-10-23 13:42:30 -0400

    prime

--- ch1/1.28.rkt
+++ ch1/1.28.rkt
@@ -46,10 +46,11 @@
 
 (module+ test
   (for ((i (in-range 10)))
-    (let ((b (random 100))
-          (x (random 100))
-          (m (random 100)))
-      (check = (expmod b x m) (modulo (expt b x) m)))))
+    (let ((b (random 2 100))
+          (x (random 2 100))
+          (m (random 2 100)))
+      (check-eq? (expmod b x m)
+                 (modulo (expt b x) m)))))
 
 (define fermat (probable-prime expmod))
 
@@ -72,3 +73,38 @@
 
 (module+ test
   (check-prime? (miller-rabin 10)))
+
+(define (fools-prime? with-expmod)
+  (lambda (n)
+    (let loop ((i (sub1 n)))
+      (or (< i 2)
+          (and (= i (with-expmod i n n))
+               (loop (sub1 i)))))))
+
+(define fools-fermat? (fools-prime? expmod))
+
+(define fools-miller-rabin? (fools-prime? miller-expmod))
+
+(module+ test
+  (for ((carmichael '(561 1105 1729 2465 2821 6601)))
+    (check-true (fools-fermat? carmichael))
+    (check-false (fools-miller-rabin? carmichael))))
+
+(define (search-for-primes with-prime? seq)
+  (define (now) (current-inexact-milliseconds))
+  (define (timed-prime-test n)
+    (let ((start (now)))
+      (when (with-prime? n)
+        (printf "~a is prime: ~a ms\n" n (- (now) start)))))
+  (for ((n seq))
+    (timed-prime-test n)))
+
+(module+ main
+  (require racket/dict)
+  (for (((name f)
+         (in-dict `(("trial division" . ,trial-division)
+                    ("fermat" . ,(fermat 10))
+                    ("miller-rabin" . ,(miller-rabin 10))))))
+    (printf "~a:\n" name)
+    (search-for-primes (make-prime? f)
+                       (in-range 100000 100100))))
