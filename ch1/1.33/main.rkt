f9b538145d76fb14149da791c50259b125b9cdb6
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-10-29 19:41:01 -0400

    1.33

--- ch1/1.33/main.rkt
+++ ch1/1.33/main.rkt
@@ -1,6 +1,23 @@
 #lang racket/base
 
-(provide prime?)
+(provide sum-squares-prime
+         product-relative-prime-upto
+         prime?
+         square)
+
+(require racket/function)
+
+(define (sum-squares-prime lo hi)
+  (filtered-accumulate prime? + 0 square lo add1 hi))
+
+(define (product-relative-prime-upto n)
+  (filtered-accumulate (lambda (i) (= (gcd i n) 1)) * 1 identity 1 add1 n))
+
+(define (filtered-accumulate pred combine z term lo next hi)
+  (let loop ((n lo) (acc z))
+    (cond ((> n hi) acc)
+          ((pred n) (loop (next n) (combine (term n) acc)))
+          (else (loop (next n) acc)))))
 
 (define (prime? n)
   (and (not (< n 2))
--- ch1/1.33/test.rkt
+++ ch1/1.33/test.rkt
@@ -7,6 +7,24 @@
 
 (define composites '(0 1 4 6 8 9 10 12 14 15))
 
+(define (range lo hi)
+  (for/list ((n (in-range lo (add1 hi)))) n))
+
+(test-case
+ "sum-squares-prime"
+ (for ((i (in-range 5)))
+   (let* ((lo (random 10 100))
+          (hi (+ lo (random 10 100))))
+     (check-equal? (sum-squares-prime lo hi)
+                   (apply + (map square (filter prime? (range lo hi))))))))
+
+(test-case
+ "product-relative-prime-upto"
+ (for ((i (in-range 5)))
+   (let ((n (random 10 100)))
+     (check-equal? (product-relative-prime-upto n)
+                   (apply * (filter (lambda (i) (= (gcd i n) 1)) (range 2 n)))))))
+
 (test-case
  "prime?"
  (for ((n primes))
