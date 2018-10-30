cd004b29a4546bc7a3419c31693797d1ea8be483
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-10-30 18:43:09 -0400

    1.46

---
+++ ch1/1.46/main.rkt
@@ -0,0 +1,38 @@
+#lang racket/base
+
+(provide nth-root-within)
+
+(define (nth-root-within percent n)
+  (lambda (x)
+    (let ((damped (repeated average-damp (ceiling (log n))))
+          (f (lambda (y) (/ x (expt y (sub1 n))))))
+      ((fixed-point-within percent) (damped f) 1.0))))
+
+(define (repeated f n)
+  (lambda (x)
+      (let loop ((n n) (acc x))
+        (if (zero? n)
+            acc
+            (loop (sub1 n) (f x))))))
+
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (average . args)
+  (/ (apply + args) (length args)))
+
+(define (fixed-point-within percent)
+  (lambda (f guess)
+    ((improve f (within percent)) guess)))
+
+(define (improve better good-enough?)
+  (lambda (guess)
+    (let loop ((guess guess) (next (better guess)))
+      (if (good-enough? guess next)
+          guess
+          (loop next (better next))))))
+
+(define (within percent)
+  (lambda (guess next)
+    (< (abs (/ (- next guess) guess))
+       (/ percent 100.0))))
