014a2801e2a9603a0f878c9c881344db67f04391
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-11-02 14:43:25 -0400

    2.6

--- ch2/2.6/main.rkt
+++ ch2/2.6/main.rkt
@@ -6,29 +6,26 @@
 
 (require racket/function)
 
-(define (church-0 f)
-  (lambda (x) x))
+(define (church-0 f) identity)
 
-(define (church-1 f)
-  (lambda (x) (f x)))
+(define (church-1 f) f)
 
 (define (church-2 f)
-  (lambda (x) (f (f x))))
+  (compose f f))
 
 (define (church-add1 c)
   (lambda (f)
-    (lambda (x)
-      (f ((c f) x)))))
+    (compose f (c f))))
 
 (define (number->church n)
-  ((repeated church-add1 n) church-0))
+  (lambda (f)
+    (repeated f n)))
 
 (define (repeated f n)
-  (lambda (x)
-    (let loop ((n n) (acc x))
-      (if (zero? n)
-          acc
-          (loop (sub1 n) (f acc))))))
+  (let loop ((n n) (acc identity))
+    (if (zero? n)
+        acc
+        (loop (sub1 n) (compose f acc)))))
 
 (define (church->number c)
   ((c add1) 0))
@@ -39,13 +36,11 @@
 
 (define (church-add c1 c2)
   (lambda (f)
-    (lambda (x)
-      ((c1 f) ((c2 f) x)))))
+    (compose (c1 f) (c2 f))))
 
 (define (church-mul c1 c2)
   (lambda (f)
-    (lambda (x)
-      ((c1 (c2 f)) x))))
+    (c1 (c2 f))))
 
 (define (church-expt c1 c2)
   (c2 c1))
