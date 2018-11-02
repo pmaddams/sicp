9fc08076c9d1cf1567b9956d4bebf4df8126c83e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-11-01 20:04:34 -0400

    2.6

---
+++ ch2/2.6/main.rkt
@@ -0,0 +1,45 @@
+#lang racket/base
+
+(define (church-0 f)
+  (lambda (x) x))
+
+(define (church-1 f)
+  (lambda (x) (f x)))
+
+(define (church-2 f)
+  (lambda (x) (f (f x))))
+
+(define (church-add1 c)
+  (lambda (f)
+    (lambda (x)
+      (f ((c f) x)))))
+
+(define (number->church n)
+  ((repeated church-add1 n) church-0))
+
+(define (repeated f n)
+  (lambda (x)
+    (let loop ((n n) (acc x))
+      (if (zero? n)
+          acc
+          (loop (sub1 n) (f acc))))))
+
+(define (church->number c)
+  ((c add1) 0))
+
+(define (church-= c1 c2)
+  (= (church->number c1)
+     (church->number c2)))
+
+(define (church-add c1 c2)
+  (lambda (f)
+    (lambda (x)
+      ((c1 f) ((c2 f) x)))))
+
+(define (church-mul c1 c2)
+  (lambda (f)
+    (lambda (x)
+      ((c1 (c2 f)) x))))
+
+(define (church-expt c1 c2)
+  (c2 c1))
