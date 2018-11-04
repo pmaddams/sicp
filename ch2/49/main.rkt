892ae9627ae2160972caed69b7d6d5beb714a006
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-11-04 11:22:09 -0500

    2.49

--- ch2/49/main.rkt
+++ ch2/49/main.rkt
@@ -7,19 +7,24 @@
 
 (define size 300)
 
+(define (paint painter)
+  (painter unit-frame)
+  (show))
+
+(define (make-painter . args)
+  (segments->painter
+   (apply append (map make-segment args))))
+
+(define (make-segment l)
+  (points->segments
+   (for/list ((coords l))
+     (apply point coords))))
+
+(define (show) image)
+
 (define image (make-bitmap size size))
 
 (define dc (make-object bitmap-dc% image))
-
-(define (draw points)
-  ((segments->painter
-    (points->segments
-     (for/list ((l points))
-       (apply point l))))
-   unit-frame)
-  (show))
-
-(define (show) image)
 
 (define (reset)
   (set! image (make-bitmap size size))
@@ -41,6 +46,13 @@
 
 (struct segment (start end))
 
+(define (segments->painter l)
+  (lambda (fr)
+    (for ((seg l))
+      (let ((p1 ((coordinate-map fr) (segment-start seg)))
+            (p2 ((coordinate-map fr) (segment-end seg))))
+        (send dc draw-line (point-x p1) (point-y p1) (point-x p2) (point-y p2))))))
+
 (define (points->segments l)
   (if (< (length l) 2)
       (error "not enough points:" l)
@@ -48,13 +60,6 @@
         (if (null? (cdr l))
             acc
             (loop (cdr l) (cons (segment (car l) (cadr l)) acc))))))
-
-(define (segments->painter l)
-  (lambda (fr)
-    (for ((seg l))
-      (let ((p1 ((coordinate-map fr) (segment-start seg)))
-            (p2 ((coordinate-map fr) (segment-end seg))))
-        (send dc draw-line (point-x p1) (point-y p1) (point-x p2) (point-y p2))))))
 
 (struct frame (origin e1 e2))
 
@@ -76,3 +81,11 @@
     (point-add (frame-origin fr)
                (point-add (point-scale (point-x pt) (frame-e1 fr))
                           (point-scale (point-y pt) (frame-e2 fr))))))
+
+(define wave
+  (make-painter
+   '((0.00 0.85) (0.15 0.60) (0.30 0.65) (0.40 0.65) (0.35 0.85) (0.40 1.00))
+   '((0.60 1.00) (0.65 0.85) (0.60 0.65) (0.75 0.65) (1.00 0.35))
+   '((1.00 0.15) (0.60 0.45) (0.75 0.00))
+   '((0.60 0.00) (0.50 0.30) (0.40 0.00))
+   '((0.25 0.00) (0.35 0.50) (0.30 0.60) (0.15 0.40) (0.00 0.65))))
