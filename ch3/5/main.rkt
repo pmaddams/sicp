6d694553d6f000d45d62464828243017dc519068
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-11-07 19:07:06 -0500

    3.5

--- ch3/5/main.rkt
+++ ch3/5/main.rkt
@@ -3,7 +3,9 @@
 ; Exercise 3.5: Monte Carlo method
 
 (define (estimate-integral make-experiment x1 x2 y1 y2 trials)
-  (monte-carlo trials (make-experiment x1 x2 y1 y2)))
+  (* (monte-carlo trials (make-experiment x1 x2 y1 y2))
+     (- x2 x1)
+     (- y2 y1)))
 
 (define (monte-carlo trials experiment)
   (let loop ((remaining trials) (passed 0))
@@ -11,15 +13,18 @@
           ((experiment) (loop (sub1 remaining) (add1 passed)))
           (else (loop (sub1 remaining) passed)))))
 
+(define (estimate-pi trials)
+  (estimate-integral make-circle-experiment -1.0 1.0 -1.0 1.0 trials))
+
 (define (make-circle-experiment x1 x2 y1 y2)
   (let ((radius (/ (- x2 x1) 2)))
     (if (not (= radius (/ (- y2 y1) 2)))
         (error "invalid bounds:" x1 x2 y1 y2)
         (lambda ()
-      (let ((x (random-in-range x1 x2))
-            (y (random-in-range y1 y2)))
-        (<= (+ (square x) (square y))
-            (square radius)))))))
+          (let ((x (random-in-range x1 x2))
+                (y (random-in-range y1 y2)))
+            (<= (+ (square x) (square y))
+                (square radius)))))))
 
 (define (random-in-range lo hi)
   (+ lo (* (random) (- hi lo))))
