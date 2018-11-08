5e7df00fb6f1899abe079b1b52a2ba7c28ba74b5
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2018-11-08 14:36:01 -0500

    3.19

---
+++ ch3/19/main.rkt
@@ -0,0 +1,14 @@
+#lang racket/base
+
+; Exercise 3.19: Finding cycles
+
+(define (has-cycle? l)
+  (define (next l)
+    (if (null? l) '() (cdr l)))
+
+  (let loop ((l1 l) (l2 (next l)))
+    (and (not (null? l1))
+         (not (null? l2))
+         (or (eq? l1 l2)
+             (eq? l1 (next l2))
+             (loop (next l1) (next (next l2)))))))
