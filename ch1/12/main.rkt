3fb7b86578be4c194086b8b408bb0891feeb4bc3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-18 17:45:44 -0500

    1.12

--- ch1/12/main.rkt
+++ ch1/12/main.rkt
@@ -1,11 +1,18 @@
 #lang racket/base
 
-; Exercise 1.12: Pascal's triangle
+; Exercise 1.12
 
-(provide choose)
+(provide pascal)
+
+(require racket/sequence)
+
+(define (pascal n)
+  (for/list ((k (in-range (add1 n))))
+    (choose n k)))
 
 (define (choose n k)
-  (if (or (zero? n) (zero? k) (= n k))
-      1
-      (+ (choose (sub1 n) (sub1 k))
-         (choose (sub1 n) k))))
+  (quotient (product (add1 k) n)
+            (product 2 (- n k))))
+
+(define (product lo hi)
+  (sequence-fold * 1 (in-range lo (add1 hi))))
--- ch1/12/test.rkt
+++ ch1/12/test.rkt
@@ -3,16 +3,12 @@
 (require rackunit
          "main.rkt")
 
-(define (row n)
-  (for/list ((k (in-range (add1 n))))
-    (choose n k)))
-
 (test-case
- "choose"
+ "pascal"
  (for ((n (in-range 5))
        (l '((1)
             (1 1)
             (1 2 1)
             (1 3 3 1)
             (1 4 6 4 1))))
-   (check-equal? (row n) l)))
+   (check-equal? (pascal n) l)))
