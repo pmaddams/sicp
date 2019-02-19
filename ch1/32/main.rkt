016c934d2486ec12ea3c769a7ee4924e656216b3
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-19 18:49:12 -0500

    1.32

---
+++ ch1/32/main.rkt
@@ -0,0 +1,22 @@
+#lang racket/base
+
+; Exercise 1.32
+
+(provide sum product)
+
+(require racket/generator
+         racket/sequence)
+
+(define (sum term a next b)
+  (accumulate + 0 term a next b))
+
+(define (product term a next b)
+  (accumulate * 1 term a next b))
+
+(define (accumulate f z term a next b)
+  (sequence-fold f z (generate term a next b)))
+
+(define (generate term a next b)
+  (let ((g (infinite-generator (yield a) (set! a (next a))))
+        (p (lambda (a) (> a b))))
+    (sequence-map term (in-producer g p))))
