f6634eb27cd7a5c1f37313fd5babb87ba977e9ed
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-18 19:24:12 -0500

    1.19

--- ch1/19/main.rkt
+++ ch1/19/main.rkt
@@ -1,20 +1,20 @@
 #lang racket/base
 
-; Exercise 1.19: Fibonacci numbers
+; Exercise 1.19
 
 (provide fib)
 
 (define (fib n)
   (let loop ((n n) (a 1) (b 0) (p 0) (q 1))
     (cond ((zero? n) b)
-          ((even? n) (loop (/ n 2) a b (next-p p q) (next-q p q)))
-          (else (loop (sub1 n) (next-a a b p q) (next-b a b p q) p q)))))
-
-(define (next-a a b p q)
-  (+ (* (+ p q) a) (* q b)))
-
-(define (next-b a b p q)
-  (+ (* q a) (* p b)))
+          ((even? n)
+           (loop (/ n 2) a b (next-p p q) (next-q p q)))
+          (else
+           (loop (sub1 n)
+                 (+ (* (+ p q) a) (* q b))
+                 (+ (* q a) (* p b))
+                 p
+                 q)))))
 
 (define (next-p p q)
   (+ (square p) (square q)))
@@ -22,5 +22,4 @@
 (define (next-q p q)
   (+ (* 2 p q) (square q)))
 
-(define (square n)
-  (* n n))
+(define (square n) (* n n))
