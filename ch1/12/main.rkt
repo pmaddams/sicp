e3444da1dba7ce9e927f253a28de9eb8f7e9059a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-19 11:44:30 -0500

    1.12

--- ch1/12/main.rkt
+++ ch1/12/main.rkt
@@ -4,15 +4,15 @@
 
 (provide pascal)
 
-(require racket/sequence)
-
 (define (pascal n)
   (for/list ((k (in-range (add1 n))))
     (choose n k)))
 
 (define (choose n k)
-  (quotient (product (add1 k) n)
-            (product 2 (- n k))))
+  (quotient (factorial (add1 k) n)
+            (factorial (- n k))))
 
-(define (product lo hi)
-  (sequence-fold * 1 (in-range lo (add1 hi))))
+(define factorial
+  (case-lambda
+    ((hi) (factorial 2 hi))
+    ((lo hi) (for/product ((n (in-range lo (add1 hi)))) n))))
