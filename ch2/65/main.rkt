40124366f9d2474d58bac1d0bd7c1662e9a3047f
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-21 17:23:10 -0500

    2.65

--- ch2/65/main.rkt
+++ ch2/65/main.rkt
@@ -1,8 +1,28 @@
 #lang racket/base
 
-; Exercise 2.65: Set representation
+; Exercise 2.65
 
 (struct tree (val left right))
+
+(define (list->tree l)
+  (car (let loop ((l l) (n (length l)))
+         (if (zero? n)
+             (cons '() l)
+             (let* ((left-size (quotient (sub1 n) 2))
+                    (left-pair (loop l left-size))
+                    (non-left-l (cdr left-pair))
+                    (right-size (- n (add1 left-size)))
+                    (right-pair (loop (cdr non-left-l) right-size)))
+               (cons (tree (car non-left-l) (car left-pair) (car right-pair))
+                     (cdr right-pair)))))))
+
+(define (tree->list t)
+  (let loop ((t t) (acc '()))
+    (if (null? t)
+        acc
+        (loop (tree-left t)
+              (cons (tree-val t)
+                    (loop (tree-right t) acc))))))
 
 (define (element? n t)
   (let loop ((t t))
@@ -21,25 +41,16 @@
                 ((> n val) (tree val (tree-right t) (loop (tree-right t))))
                 (else t))))))
 
-(define (tree->list t)
-  (let loop ((t t) (acc '()))
-    (if (null? t)
-        acc
-        (loop (tree-left t)
-              (cons (tree-val t)
-                    (loop (tree-right t) acc))))))
-
-(define (list->tree l)
-  (car (let loop ((l l) (n (length l)))
-         (if (zero? n)
-             (cons '() l)
-             (let* ((left-size (quotient (sub1 n) 2))
-                    (left-pair (loop l left-size))
-                    (non-left-l (cdr left-pair))
-                    (right-size (- n (add1 left-size)))
-                    (right-pair (loop (cdr non-left-l) right-size)))
-               (cons (tree (car non-left-l) (car left-pair) (car right-pair))
-                     (cdr right-pair)))))))
+(define (union t1 t2)
+  (list->tree
+   (let loop ((l1 (tree->list t1)) (l2 (tree->list t2)))
+     (cond ((null? l1) l2)
+           ((null? l2) l1)
+           (else (let ((n1 (car l1))
+                       (n2 (car l2)))
+                   (cond ((< n1 n2) (cons n1 (loop (cdr l1) l2)))
+                         ((> n1 n2) (cons n2 (loop l1 (cdr l2))))
+                         (else (cons n1 (loop (cdr l1) (cdr l2)))))))))))
 
 (define (intersect t1 t2)
   (list->tree
@@ -52,14 +63,3 @@
            (cond ((< n1 n2) (loop (cdr l1) l2))
                  ((> n1 n2) (loop l1 (cdr l2)))
                  (else (cons n1 (loop (cdr l1) (cdr l2))))))))))
-
-(define (union t1 t2)
-  (list->tree
-   (let loop ((l1 (tree->list t1)) (l2 (tree->list t2)))
-     (cond ((null? l1) l2)
-           ((null? l2) l1)
-           (else (let ((n1 (car l1))
-                       (n2 (car l2)))
-                   (cond ((< n1 n2) (cons n1 (loop (cdr l1) l2)))
-                         ((> n1 n2) (cons n2 (loop l1 (cdr l2))))
-                         (else (cons n1 (loop (cdr l1) (cdr l2)))))))))))
