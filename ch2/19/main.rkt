1d531f301fd2d65ecba6d194392770508eb1c6a4
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-20 19:02:23 -0500

    2.19

--- ch2/19/main.rkt
+++ ch2/19/main.rkt
@@ -1,21 +1,13 @@
 #lang racket/base
 
-; Exercise 2.19: Counting change
+; Exercise 2.19
 
 (define us-coins '(1 5 10 25 50))
 
 (define uk-coins '(0.5 1 2 5 10 20 50 100))
 
-(define (count amount coins)
-  (let loop ((amount amount) (coins (dedup coins)))
-    (cond ((or (negative? amount) (null? coins)) 0)
-          ((zero? amount) 1)
-          (else (+ (loop amount (cdr coins))
-                   (loop (- amount (car coins)) coins))))))
-
-(define (dedup l)
-  (let loop ((l l) (acc '()))
-    (if (null? l)
-        acc
-        (loop (remove* (list (car l)) (cdr l))
-              (cons (car l) acc)))))
+(define (change amount coins)
+  (cond ((or (negative? amount) (null? coins)) 0)
+        ((zero? amount) 1)
+        (else (+ (change amount (cdr coins))
+                 (change (- amount (car coins)) coins)))))
