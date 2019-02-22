ba598d02a0fed4785a0bf2c4e5c35f05e70be517
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-21 19:17:00 -0500

    2.85

--- ch2/85/main.rkt
+++ ch2/85/main.rkt
@@ -1,263 +1,297 @@
 #lang racket/base
 
-; Exercise 2.85: Generic arithmetic
+; Exercise 2.85
 
 (require racket/function
          racket/list)
 
-(define tab (make-hash))
+(struct number (type val))
 
-(define (put k v)
-  (hash-set! tab k v))
+(define table (make-hash))
 
-(define (get k)
-  (hash-ref tab k))
+(define (put k v) (hash-set! table k v))
 
-(define (add n m)
-  (apply-generic 'add n m))
+(define (get k) (hash-ref table k))
 
-(define (sub n m)
-  (apply-generic 'sub n m))
+(define (add n m) (apply-generic 'add n m))
 
-(define (mul n m)
-  (apply-generic 'mul n m))
+(define (sub n m) (apply-generic 'sub n m))
 
-(define (div n m)
-  (apply-generic 'div n m))
+(define (mul n m) (apply-generic 'mul n m))
+
+(define (div n m) (apply-generic 'div n m))
 
 (define (apply-generic op . args)
-  (let ((args (coerce args)))
-    (simpl (apply (get (list op (type-tag (car args)))) args))))
+  (let* ((args* (coerce args))
+         (t (number-type (car args*)))
+         (f (get (cons op t))))
+    (simplify (apply f args*))))
 
 (define (coerce args)
-  (let* ((levels (map type-level args))
+  (let* ((levels (map level args))
          (top (apply max levels)))
-    (let loop ((args args) (levels levels) (acc '()))
-      (if (null? args)
-          acc
-          (loop (cdr args)
-                (cdr levels)
-                (cons ((repeated super (- top (car levels))) (car args))
-                      acc))))))
+    (for/fold ((acc '()))
+              ((a (in-list args))
+               (l (in-list levels)))
+      (cons ((repeated super (- top l)) a) acc))))
 
-(define (type-tag n)
-  (car (type n)))
-
-(define (type-level n)
-  (cdr (type n)))
-
-(define (type n)
-  (cond ((int? n) '(int . 0))
-        ((rat? n) '(rat . 1))
-        ((re? n) '(re . 2))
-        ((cmplx? n) '(cmplx . 3))
-        (else (error "unknown type:" n))))
-
-(define (repeated f n)
-  (let loop ((n n) (acc identity))
-    (if (zero? n)
-        acc
-        (loop (sub1 n) (compose f acc)))))
+(define (level n)
+  (let ((t (number-type n)))
+    (index-of '(integer rational real complex) t)))
 
 (define (super n)
-  ((get `(super ,(type-tag n))) n))
+  (let* ((t (number-type n))
+         (f (get (cons 'super t))))
+    (f n)))
 
-(define (simpl n)
-  ((get `(simpl ,(type-tag n))) n))
+(define (simplify n)
+  (let* ((t (number-type n))
+         (f (get (cons 'simplify t))))
+    (f n)))
 
 (define (show n)
-  ((get `(show ,(type-tag n))) n))
+  (let* ((t (number-type n))
+         (f (get (cons 'show t))))
+    (f n)))
 
-(struct int (val))
+(define (make-integer v)
+  (number 'integer v))
 
-(define (install-int-package)
-  (put '(add int)
+(define (integer? n)
+  (eq? 'integer (number-type n)))
+
+(define (install-integer-package)
+  (put '(add . integer)
        (lambda (n m)
-         (int (+ (int-val n) (int-val m)))))
+         (make-integer
+          (+ (number-val n) (number-val m)))))
 
-  (put '(sub int)
+  (put '(sub . integer)
        (lambda (n m)
-         (int (- (int-val n) (int-val m)))))
+         (make-integer
+          (- (number-val n) (number-val m)))))
 
-  (put '(mul int)
+  (put '(mul . integer)
        (lambda (n m)
-         (int (* (int-val n) (int-val m)))))
+         (make-integer
+          (* (number-val n) (number-val m)))))
 
-  (put '(div int)
+  (put '(div . integer)
        (lambda (n m)
-         (int (/ (int-val n) (int-val m)))))
+         (make-integer
+          (/ (number-val n) (number-val m)))))
 
-  (put '(super int)
+  (put '(super . integer)
        (lambda (n)
-         (rat (int-val n) 1)))
+         (make-rational
+          (number-val n)
+          1)))
 
-  (put '(simpl int)
+  (put '(simplify . integer)
        (lambda (n)
-         (int (inexact->exact (int-val n)))))
+         (make-integer
+          (inexact->exact (number-val n)))))
 
-  (put '(show int)
+  (put '(show . integer)
        (lambda (n)
-         (number->string (int-val n)))))
+         (number->string (number-val n)))))
 
-(install-int-package)
+(install-integer-package)
 
-(struct rat (numer denom))
+(define (make-rational n d)
+  (let* ((g (gcd n d))
+         (n* (/ n g))
+         (d* (/ d g)))
+    (if (negative? d*)
+        (number 'rational (cons (- n*) (- d*)))
+        (number 'rational (cons n* d*)))))
 
-(define (make-rat numer denom)
-  (let* ((g (gcd numer denom))
-         (numer (/ numer g))
-         (denom (/ denom g)))
-    (if (negative? denom)
-        (rat (- numer) (- denom))
-        (rat numer denom))))
+(define (rational? n)
+  (eq? 'rational (number-type n)))
 
-(define (install-rat-package)
-  (put '(add rat)
+(define (numer n)
+  (if (rational? n)
+      (car (number-val n))
+      (error "not a rational number")))
+
+(define (denom n)
+  (if (rational? n)
+      (cdr (number-val n))
+      (error "not a rational number")))
+
+(define (install-rational-package)
+  (put '(add . rational)
        (lambda (n m)
-         (make-rat (+ (* (rat-numer n) (rat-denom m))
-                      (* (rat-denom n) (rat-numer m)))
-                   (* (rat-denom n) (rat-denom m)))))
+         (make-rational
+          (+ (* (numer n) (denom m))
+             (* (denom n) (numer m)))
+          (* (denom n) (denom m)))))
 
-  (put '(sub rat)
+  (put '(sub . rational)
        (lambda (n m)
-         (make-rat (- (* (rat-numer n) (rat-denom m))
-                      (* (rat-denom n) (rat-numer m)))
-                   (* (rat-denom n) (rat-denom m)))))
+         (make-rational
+          (- (* (numer n) (denom m))
+             (* (denom n) (numer m)))
+          (* (denom n) (denom m)))))
 
-  (put '(mul rat)
+  (put '(mul . rational)
        (lambda (n m)
-         (make-rat (* (rat-numer n) (rat-numer m))
-                   (* (rat-denom n) (rat-denom m)))))
+         (make-rational
+          (* (numer n) (numer m))
+          (* (denom n) (denom m)))))
 
-  (put '(div rat)
+  (put '(div . rational)
        (lambda (n m)
-         (make-rat (* (rat-numer n) (rat-denom m))
-                   (* (rat-denom n) (rat-numer m)))))
+         (make-rational
+          (* (numer n) (denom m))
+          (* (denom n) (numer m)))))
 
-  (put '(super rat)
+  (put '(super . rational)
        (lambda (n)
-         (let ((int->re (lambda (v) (+ v 0.0))))
-           (/ (int->re (rat-numer n))
-              (int->re (rat-denom n))))))
+         (make-real
+          (/ (numer n) (denom n)))))
 
-  (put '(simpl rat)
+  (put '(simplify . rational)
        (lambda (n)
-         (let ((n (make-rat (rat-numer n) (rat-denom n))))
-           (if (= (rat-denom n) 1)
-               (rat-numer n)
-               n))))
+         (let ((n* (make-rational (numer n) (denom n))))
+           (if (= 1 (denom n*))
+               (make-integer (numer n*))
+               n*))))
 
-  (put '(show rat)
+  (put '(show . rational)
        (lambda (n)
          (string-append
-          (number->string (rat-numer n))
+          (number->string (numer n))
           "/"
-          (number->string (rat-denom n))))))
+          (number->string (denom n))))))
 
-(install-rat-package)
+(install-rational-package)
 
-(struct re (val))
+(define (make-real v)
+  (number 'real v))
 
-(define (install-re-package)
-  (put '(add re)
+(define (real? n)
+  (eq? 'real (number-type n)))
+
+(define (install-real-package)
+  (put '(add . real)
        (lambda (n m)
-         (re (+ (re-val n) (re-val m)))))
+         (make-real
+          (+ (number-val n) (number-val m)))))
 
-  (put '(sub re)
+  (put '(sub . real)
        (lambda (n m)
-         (re (- (re-val n) (re-val m)))))
+         (make-real
+          (- (number-val n) (number-val m)))))
 
-  (put '(mul re)
+  (put '(mul . real)
        (lambda (n m)
-         (re (* (re-val n) (re-val m)))))
+         (make-real
+          (* (number-val n) (number-val m)))))
 
-  (put '(div re)
+  (put '(div . real)
        (lambda (n m)
-         (re (/ (re-val n) (re-val m)))))
+         (make-real
+          (/ (number-val n) (number-val m)))))
 
-  (put '(super re)
+  (put '(super . real)
        (lambda (n)
-         (cmplx (re-val n) 0.0)))
+         (make-rectangular
+          (number-val n)
+          0.0)))
 
-  (put '(simpl re)
+  (put '(simplify . real)
        (lambda (n)
-         (let ((v (re-val n)))
-           (if (= (round v) v) (int v) n))))
+         (let ((v (number-val n)))
+           (if (= v (round v)) (make-integer v) n))))
 
-  (put '(show re)
+  (put '(show . real)
        (lambda (n)
-         (number->string (re-val n)))))
+         (number->string (number-val n)))))
 
-(install-re-package)
+(install-real-package)
 
-(struct rect (re im))
+(define (make-rectangular r i)
+  (number 'rectangular (cons r i)))
 
-(struct polar (mag ang))
+(define (rectangular? n)
+  (eq? 'rectangular (number-type n)))
 
-(define (cmplx re im)
-  (rect re im))
+(define (make-polar m a)
+  (number 'polar m a))
 
-(define (cmplx? n)
-  (or (rect? n) (polar? n)))
+(define (polar? n)
+  (eq? 'polar (number-type n)))
 
-(define (cmplx-re n)
-  (if (rect? n)
-      (rect-re n)
-      (* (polar-mag n) (cos (polar-ang n)))))
+(define (complex? n)
+  (or (rectangular? n) (polar? n)))
 
-(define (cmplx-im n)
-  (if (rect? n)
-      (rect-im n)
-      (* (polar-mag n) (sin (polar-ang n)))))
+(define (real-part n)
+  (cond ((rectangular? n) (car (number-val n)))
+        ((polar? n) (* (magnitude n) (cos (angle n))))
+        (else (error "not a complex number"))))
 
-(define (cmplx-mag n)
-  (if (polar? n)
-      (polar-mag n)
-      (let ((square (lambda (v) (* v v))))
-        (sqrt (+ (square (rect-re n))
-                 (square (rect-im n)))))))
+(define (imag-part n)
+  (cond ((rectangular? n) (cdr (number-val n)))
+        ((polar? n) (* (magnitude n) (sin (angle n))))
+        (else (error "not a complex number"))))
 
-(define (cmplx-ang n)
-  (if (polar? n)
-      (polar-mag n)
-      (atan (rect-im n)
-            (rect-re n))))
+(define (magnitude n)
+  (cond ((rectangular? n) (let ((r (real-part n))
+                                (i (imag-part n)))
+                            (sqrt (+ (* r r) (* i i)))))
+        ((polar? n) (car (number-val n)))
+        (else (error "not a complex number"))))
 
-(define (install-cmplx-package)
-  (put '(add cmplx)
+(define (angle n)
+  (cond ((rectangular? n) (atan (real-part n) (imag-part n)))
+        ((polar? n) (cdr (number-val n)))
+        (else (error "not a complex number"))))
+
+(define (install-complex-package)
+  (put '(add . complex)
        (lambda (n m)
-         (rect (+ (cmplx-re n) (cmplx-re m))
-               (+ (cmplx-im n) (cmplx-im m)))))
+         (make-rectangular
+          (+ (real-part n) (real-part m))
+          (+ (imag-part n) (imag-part m)))))
 
-  (put '(sub cmplx)
+  (put '(sub . complex)
        (lambda (n m)
-         (rect (- (cmplx-re n) (cmplx-re m))
-               (- (cmplx-im n) (cmplx-im m)))))
+         (make-rectangular
+          (- (real-part n) (real-part m))
+          (- (imag-part n) (imag-part m)))))
 
-  (put '(mul cmplx)
+  (put '(mul . complex)
        (lambda (n m)
-         (polar (* (cmplx-mag n) (cmplx-mag m))
-                (+ (cmplx-ang n) (cmplx-ang m)))))
+         (make-polar
+          (* (magnitude n) (magnitude m))
+          (+ (angle n) (angle m)))))
 
-  (put '(div cmplx)
+  (put '(div . complex)
        (lambda (n m)
-         (polar (/ (cmplx-mag n) (cmplx-mag m))
-                (- (cmplx-ang n) (cmplx-ang m)))))
+         (make-polar
+          (/ (magnitude n) (magnitude m))
+          (- (angle n) (angle m)))))
 
-  (put '(simpl cmplx)
+  (put '(simplify . complex)
        (lambda (n)
-         (let ((re (simpl (cmplx-re n)))
-               (im (simpl (cmplx-im n))))
-           (if (zero? im)
-               re
-               (cmplx re im)))))
+         (let ((r (real-part n))
+               (i (imag-part n)))
+           (if (zero? i)
+               r
+               (make-rectangular r i)))))
 
-  (put '(show cmplx)
+  (put '(show . complex)
        (lambda (n)
          (string-append
-          (number->string (cmplx-re n))
+          (number->string (real-part n))
           "+"
-          (number->string (cmplx-im n))
+          (number->string (imag-part n))
           "i"))))
 
-(install-cmplx-package)
+(install-complex-package)
+
+(define (repeated f n)
+  (for/fold ((acc identity))
+            ((i (in-range n)))
+    (compose f acc)))
