f3c3e0f8ce1350c07fa2a02f7000b4a6eaab64e5
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-25 16:28:42 -0500

    3.47

--- ch3/47/main.rkt
+++ ch3/47/main.rkt
@@ -1,48 +1,57 @@
 #lang racket/base
 
-; Exercise 3.47: Semaphores
+; Exercise 3.47
 
 (provide (all-defined-out))
 
 (require ffi/unsafe/atomic
-         racket/class)
+         racket/class
+         racket/function)
 
 (define-syntax-rule (concurrent expr ...)
-  (for-each thread (list (lambda () expr) ...)))
+  (for-each thread (list (thunk expr) ...)))
 
 (define cell%
   (class object%
     (super-new)
+
     (field (val #f))
 
     (define/public (test-and-set)
       (call-as-atomic
-       (lambda ()
-         (let ((old-val val))
-           (set! val #t)
-           old-val))))
+       (thunk
+        (let ((old-val val))
+          (set! val #t)
+          old-val))))
 
     (define/public (clear)
       (set! val #f))))
 
-(define (test-and-set cell)
-  (send cell test-and-set))
-
-(define (clear cell)
-  (send cell clear))
-
-(define mutex%
+(define semaphore%
   (class cell%
     (super-new)
 
-    (define/public (lock)
-      (when (test-and-set this) (lock)))
+    (init-field size)
+    (field (count 0))
 
-    (define/public (unlock)
-      (clear this))))
+    (define/public (acquire)
+      (if (or (>= count size)
+              (send this test-and-set))
+          (acquire)
+          (begin (set! count (add1 count))
+                 (send this clear))))
 
-(define (lock mutex)
-  (send mutex lock))
+    (define/public (release)
+      (unless (zero? count)
+        (if (send this test-and-set)
+            (release)
+            (begin (set! count (sub1 count))
+                   (send this clear)))))))
 
-(define (unlock mutex)
-  (send mutex unlock))
+(define mutex%
+  (class semaphore%
+    (super-new (size 1))))
+
+(define (acquire x) (send x acquire))
+
+(define (release x) (send x release))
