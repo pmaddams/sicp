3dff96da9585023a6589888154f1e87eaeb3a545
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-02-27 12:07:21 -0500

    3.56

---
+++ ch3/56/main.rkt
@@ -0,0 +1,32 @@
+#lang racket/base
+
+; Exercise 3.56
+
+(require racket/stream)
+
+(define hamming
+  (stream-cons 1 (merge (scale hamming 2)
+                        (scale hamming 3)
+                        (scale hamming 5))))
+
+(define (merge . args)
+  (letrec ((loop (lambda (s1 s2)
+                   (cond ((stream-empty? s1) s2)
+                         ((stream-empty? s2) s1)
+                         (else
+                          (let ((a (stream-first s1))
+                                (b (stream-first s2)))
+                            (cond ((< a b)
+                                   (stream-cons a (loop (stream-rest s1)
+                                                        s2)))
+                                  ((> a b)
+                                   (stream-cons b (loop s1
+                                                        (stream-rest s2))))
+                                  (else
+                                   (stream-cons a (loop (stream-rest s1)
+                                                        (stream-rest s2)))))))))))
+    (foldr loop empty-stream args)))
+
+(define (scale s n)
+  (for/stream ((i s))
+    (* i n)))
