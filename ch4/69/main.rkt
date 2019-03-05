8534aa46b9d6a819f895b657246a8bd1605b0128
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-03-04 20:45:21 -0500

    4.69

--- ch4/69/main.rkt
+++ ch4/69/main.rkt
@@ -3,3 +3,14 @@
 ; Exercise 4.69
 
 (provide (all-defined-out))
+
+(require racket/promise
+         racket/stream)
+
+(struct expr (type val))
+
+(define table (make-hash))
+
+(define (put k v) (hash-set! table k v))
+
+(define (get k) (hash-ref table k))
