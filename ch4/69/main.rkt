51cedfa1c3199f15fad8f343da773c61a5d76376
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-03-04 21:06:05 -0500

    4.69

--- ch4/69/main.rkt
+++ ch4/69/main.rkt
@@ -14,3 +14,29 @@
 (define (put k v) (hash-set! table k v))
 
 (define (get k) (hash-ref table k))
+
+(define (stream-append-map f s)
+  (stream-flatten (stream-map f s)))
+
+(define (stream-flatten s)
+  (if (stream-empty? s)
+      empty-stream
+      (interleave-delayed
+       (stream-first s)
+       (delay (stream-flatten (stream-rest s))))))
+
+(define (interleave-delayed s ds)
+  (if (stream-empty? s)
+      (force ds)
+      (stream-cons (stream-first s)
+                   (interleave-delayed
+                    (force ds)
+                    (delay (stream-rest s))))))
+
+(define (stream-append-delayed s ds)
+  (if (stream-empty? s)
+      (force ds)
+      (stream-cons (stream-first s)
+                   (stream-append-delayed
+                    (stream-rest s)
+                    ds))))
