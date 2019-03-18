81ad6e610ad76c2e2c698f458947133f494a4bbc
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-03-17 20:46:14 -0400

    5.7

--- ch5/07/main.rkt
+++ ch5/07/main.rkt
@@ -7,44 +7,37 @@
 (require racket/class
          "../../vm/vm.rkt")
 
-(define (expt-rec a n)
-  (define code
-    '((assign continue (label done))
-      loop
-      (test (op zero?) (reg n))
-      (branch (label base-case))
-      (assign n (op sub1) (reg n))
-      (save continue)
-      (assign continue (label after-loop))
-      (goto (label loop))
-      after-loop
-      (restore continue)
-      (assign val (op *) (reg val) (reg a))
-      (goto (reg continue))
-      base-case
-      (assign val (const 1))
-      (goto (reg continue))
-      done))
-
+(define ((make-expt code) a n)
   (let ((vm (make-vm code)))
     (send vm set 'a a)
     (send vm set 'n n)
     (send vm execute)
     (send vm get 'val)))
 
-(define (expt-iter a n)
-  (define code
-    '((assign val (const 1))
-      loop
-      (test (op zero?) (reg n))
-      (branch (label done))
-      (assign val (op *) (reg a) (reg val))
-      (assign n (op sub1) (reg n))
-      (goto (label loop))
-      done))
+(define recursive-process
+  '((assign continue (label done))
+    loop
+    (test (op zero?) (reg n))
+    (branch (label base-case))
+    (save continue)
+    (assign n (op sub1) (reg n))
+    (assign continue (label after-loop))
+    (goto (label loop))
+    after-loop
+    (restore continue)
+    (assign val (op *) (reg a) (reg val))
+    (goto (reg continue))
+    base-case
+    (assign val (const 1))
+    (goto (reg continue))
+    done))
 
-  (let ((vm (make-vm code)))
-    (send vm set 'a a)
-    (send vm set 'n n)
-    (send vm execute)
-    (send vm get 'val)))
+(define iterative-process
+  '((assign val (const 1))
+    loop
+    (test (op zero?) (reg n))
+    (branch (label done))
+    (assign val (op *) (reg a) (reg val))
+    (assign n (op sub1) (reg n))
+    (goto (label loop))
+    done))
