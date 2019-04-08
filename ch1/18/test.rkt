a2b8f97f2671883edbe52cd27af8288858457987
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-08 11:44:20 -0400

    1.18

--- ch1/18/test.rkt
+++ ch1/18/test.rkt
@@ -3,21 +3,11 @@
 (require rackunit
          "main.rkt")
 
-(define-check (check-op f reference)
-  (for ((i (in-range 5)))
-    (let* ((n (random 1 5))
-           (m (random 1 5))
-           (expected (reference n m))
-           (actual (f n m)))
-      (with-check-info (('actual actual) ('expected expected))
-        (unless (= actual expected) (fail-check))))))
-
-(test-case "add" (check-op add +))
-
-(test-case "sub" (check-op sub -))
-
-(test-case "mul" (check-op mul *))
-
-(test-case "div" (check-op div quotient))
-
-(test-case "pow" (check-op pow expt))
+(test-case
+ "arithmetic"
+ (for ((f (in-list (list add sub mul div pow)))
+       (g (in-list (list + - * quotient expt))))
+   (for ((i (in-range 5)))
+     (let ((n (random 1 10))
+           (m (random 1 10)))
+       (check-equal? (f n m) (g n m))))))
