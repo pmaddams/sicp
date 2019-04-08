a2311f1cd54cee3f763dab02512a2465f9ed55ab
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-08 11:35:48 -0400

    1.10

--- ch1/10/test.rkt
+++ ch1/10/test.rkt
@@ -3,15 +3,9 @@
 (require rackunit
          "main.rkt")
 
-(define-check (check-A i f)
-  (for ((n (in-range 5)))
-    (let ((expected (A i n))
-          (actual (f n)))
-      (with-check-info (('actual actual) ('expected expected))
-        (unless (= actual expected) (fail-check))))))
-
-(test-case "f" (check-A 0 f))
-
-(test-case "g" (check-A 1 g))
-
-(test-case "h" (check-A 2 h))
+(test-case
+ "A"
+ (for ((proc (in-list (list f g h)))
+       (n (in-range 3)))
+   (for ((m (in-range 5)))
+     (check-equal? (A n m) (proc m)))))
