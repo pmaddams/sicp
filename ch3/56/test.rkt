125b81cd1b1ed4c6857f218afe8e8643435c7bb0
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-08 21:35:42 -0400

    3.56

---
+++ ch3/56/test.rkt
@@ -0,0 +1,10 @@
+#lang racket/base
+
+(require racket/stream
+         rackunit
+         "main.rkt")
+
+(test-case
+ "hamming"
+ (check-equal? (stream->list (stream-take hamming 20))
+               '(1 2 3 4 5 6 8 9 10 12 15 16 18 20 24 25 27 30 32 36)))
