94f7b6ea8f55ae7027f95ba27248a5f65d99559e
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-10 20:19:11 -0400

    4.6

---
+++ ch4/06/test.rkt
@@ -0,0 +1,137 @@
+#lang racket/base
+
+(require rackunit
+         "main.rkt")
+
+(test-case
+ "literals"
+ (check-equal? (interpret '(#f)) #f)
+ (check-equal? (interpret '(#t)) #t)
+ (check-equal? (interpret '(0)) 0)
+ (check-equal? (interpret '(1)) 1)
+ (let ((n (random)))
+   (check-equal? (interpret `(,n)) n)))
+
+(test-case
+ "quote"
+ (check-equal? (interpret '('foo)) 'foo)
+ (check-equal? (interpret '('(1 (2 3) 4 5))) '(1 (2 3) 4 5)))
+
+(test-case
+ "lambda"
+ (check-equal? (interpret '(((lambda (n)
+                               (* n n))
+                             2)))
+               4)
+ (check-equal? (interpret '((((lambda (n)
+                                (lambda (m)
+                                  (* n m)))
+                              2)
+                             3)))
+               6))
+
+(test-case
+ "define"
+ (check-equal? (interpret '((define (square n) (* n n))
+                            (square 2)))
+               4)
+ (check-equal? (interpret '((define n 2)
+                            (define m 3)
+                            (* n m)))
+               6))
+
+(test-case
+ "set!"
+ (check-equal? (interpret '((define n 2)
+                            (set! n (+ n 1))
+                            n))
+               3)
+ (check-equal? (interpret '((let ((n 2))
+                              (set! n (+ n 1))
+                              n)))
+               3))
+
+(test-case
+ "if"
+ (check-equal? (interpret '((if #t
+                                1
+                                (/ 1 0))))
+               1)
+ (check-equal? (interpret '((define (map f l)
+                              (if (null? l)
+                                  '()
+                                  (cons (f (car l)) (map f (cdr l)))))
+                            (map (lambda (n) (* n 2))
+                                 '(1 2 3 4 5))))
+               '(2 4 6 8 10)))
+
+(test-case
+ "begin"
+ (check-equal? (interpret '((begin #t #f)))
+               #f)
+ (check-equal? (interpret '((define l '())
+                            (begin (set! l (cons 1 l))
+                                   (set! l (cons (+ (car l) 1) (cdr l)))
+                                   (car l))))
+               2))
+
+(test-case
+ "cond"
+ (check-equal? (interpret '((define (memq x l)
+                              (cond ((null? l) #f)
+                                    ((eq? x (car l)) l)
+                                    (else (memq x (cdr l)))))
+                            (memq 3 '(1 2 3 4 5))))
+               '(3 4 5))
+ (check-equal? (interpret '((define (memq x l)
+                              (cond ((null? l) #f)
+                                    ((eq? x (car l)) l)
+                                    (else (memq x (cdr l)))))
+                            (memq 6 '(1 2 3 4 5))))
+               #f))
+
+(test-case
+ "and"
+ (check-equal? (interpret '((and)))
+               #t)
+ (check-equal? (interpret '((if (and (< 1 2)
+                                     (eq? 'a 'a)
+                                     (not (null? '(3 4 5))))
+                                'foo
+                                'bar)))
+               'foo)
+ (check-equal? (interpret '((if (and (< 1 2)
+                                     (eq? 'a 'b)
+                                     (/ 1 0))
+                                'foo
+                                'bar)))
+               'bar))
+
+(test-case
+ "or"
+ (check-equal? (interpret '((or)))
+               #f)
+ (check-equal? (interpret '((if (or (< 1 2)
+                                    (/ 1 0)
+                                    (null? (cdr '())))
+                                'foo
+                                'bar)))
+               'foo)
+ (check-equal? (interpret '((if (or (< 2 1)
+                                    (eq? 'a 'b)
+                                    (null? '(3 4 5)))
+                                'foo
+                                'bar)))
+               'bar))
+
+(test-case
+ "let"
+ (check-equal? (interpret '((let ((n 1))
+                              (let ((n 2)
+                                    (m (+ n 1)))
+                                m))))
+               2)
+ (check-equal? (interpret '((let ((square (lambda (n) (* n n))))
+                              (square 2)
+                              (square 3))))
+               9))
