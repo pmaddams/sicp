0bdac692cc24270a27958c9e2035030c634e9329
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-16 17:11:12 -0400

    1.10

--- ch1/10/test.rkt
+++ ch1/10/test.rkt
@@ -5,6 +5,12 @@
 
 (test-case
  "A"
+ (check-equal? (A 1 10) 1024)
+ (check-equal? (A 2 4) 65536)
+ (check-equal? (A 3 3) 65536))
+
+(test-case
+ "derived functions"
  (for ((proc (in-list (list f g h)))
        (n (in-range 3)))
    (for ((m (in-range 5)))
