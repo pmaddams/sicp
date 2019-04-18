cf3dbc68b4e388e0facd2539eb86a3f39c9e5f3a
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2019-04-17 20:51:58 -0400

    2.12

--- ch2/12/main.rkt
+++ ch2/12/main.rkt
@@ -7,14 +7,16 @@
 (struct interval (lo hi) #:transparent)
 
 (define (make-center-percent c %)
-  (let ((w (abs (* c (/ % 100.0)))))
-    (make-center-width c w)))
+  (validate
+   (let ((w (abs (* 0.01 % c))))
+     (make-center-width c w))))
 
 (define (percent i)
-  (* (/ (width i) (center i)) 100.0))
+  (* 100.0 (/ (width i) (center i))))
 
 (define (make-center-width c w)
-  (interval (- c w) (+ c w)))
+  (validate
+   (interval (- c w) (+ c w))))
 
 (define (center i)
   (average (interval-lo i) (interval-hi i)))
@@ -23,49 +25,58 @@
   (/ (- (interval-hi i) (interval-lo i)) 2.0))
 
 (define (interval-add i1 i2)
-  (interval (+ (interval-lo i1) (interval-lo i2))
-            (+ (interval-hi i1) (interval-hi i2))))
+  (validate
+   (interval (+ (interval-lo i1) (interval-lo i2))
+             (+ (interval-hi i1) (interval-hi i2)))))
 
 (define (interval-sub i1 i2)
-  (interval (- (interval-lo i1) (interval-hi i2))
-            (- (interval-hi i1) (interval-lo i2))))
+  (validate
+   (interval (- (interval-lo i1) (interval-hi i2))
+             (- (interval-hi i1) (interval-lo i2)))))
 
 (define (interval-mul i1 i2)
-  (let ((l1 (interval-lo i1))
-        (l2 (interval-lo i2))
-        (h1 (interval-hi i1))
-        (h2 (interval-hi i2)))
-    (cond ((not (negative? l1))
-           (cond ((not (negative? l2))
-                  (interval (* l1 l2) (* h1 h2)))
-                 ((negative? h2)
-                  (interval (* h1 l2) (* l1 h2)))
-                 (else
-                  (interval (* h1 l2) (* h1 h2)))))
-          ((negative? h1)
-           (cond ((not (negative? l2))
-                  (interval (* l1 h2) (* h1 l2)))
-                 ((negative? h2)
-                  (interval (* h1 h2) (* l1 l2)))
-                 (else
-                  (interval (* l1 h2) (* l1 l2)))))
-          (else
-           (cond ((not (negative? l2))
-                  (interval (* l1 h2) (* h1 h2)))
-                 ((negative? h2)
-                  (interval (* h1 l2) (* l1 l2)))
-                 (else
-                  (interval (min (* l1 h2) (* h1 l2))
-                            (max (* l1 l2) (* h1 h2)))))))))
+  (validate
+   (let ((l1 (interval-lo i1))
+         (l2 (interval-lo i2))
+         (h1 (interval-hi i1))
+         (h2 (interval-hi i2)))
+     (cond ((not (negative? l1))
+            (cond ((not (negative? l2))
+                   (interval (* l1 l2) (* h1 h2)))
+                  ((negative? h2)
+                   (interval (* h1 l2) (* l1 h2)))
+                  (else
+                   (interval (* h1 l2) (* h1 h2)))))
+           ((negative? h1)
+            (cond ((not (negative? l2))
+                   (interval (* l1 h2) (* h1 l2)))
+                  ((negative? h2)
+                   (interval (* h1 h2) (* l1 l2)))
+                  (else
+                   (interval (* l1 h2) (* l1 l2)))))
+           (else
+            (cond ((not (negative? l2))
+                   (interval (* l1 h2) (* h1 h2)))
+                  ((negative? h2)
+                   (interval (* h1 l2) (* l1 l2)))
+                  (else
+                   (interval (min (* l1 h2) (* h1 l2))
+                             (max (* l1 l2) (* h1 h2))))))))))
 
 (define (interval-div i1 i2)
-  (let ((l2 (interval-lo i2))
-        (h2 (interval-hi i2)))
-    (if (or (zero? l2)
-            (zero? h2)
-            (and (negative? l2) (positive? h2)))
-        (error "division by zero:" i1 i2)
-        (interval-mul i1 (interval (/ 1.0 h2) (/ 1.0 l2))))))
+  (validate
+   (let ((l2 (interval-lo i2))
+         (h2 (interval-hi i2)))
+     (if (or (zero? l2)
+             (zero? h2)
+             (and (negative? l2) (positive? h2)))
+         (error "division by zero:" i1 i2)
+         (interval-mul i1 (interval (/ 1.0 h2) (/ 1.0 l2)))))))
+
+(define (validate i)
+  (if (> (interval-lo i) (interval-hi i))
+      (error "invalid interval:" i)
+      i))
 
 (define (average . args)
   (/ (apply + args)
