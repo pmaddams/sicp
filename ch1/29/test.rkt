3549facde1ca587c7bb9b980f295687554155219
Pavan Maddamsetti <pavan.maddamsetti@gmail.com>
2020-07-28 20:43:04 -0400

    derivatives

--- ch1/29/test.rkt
+++ ch1/29/test.rkt
@@ -4,20 +4,20 @@
          rackunit
          "main.rkt")
 
-(define integrated
-  `((,(lambda (x) 1) . ,(lambda (x) x))
-    (,(lambda (x) x) . ,(lambda (x) (/ (expt x 2) 2)))
-    (,(lambda (x) (* 3 x)) . ,(lambda (x) (* (/ 3 2) (expt x 2))))
-    (,(lambda (x) (expt x 3)) . ,(lambda (x) (/ (expt x 4) 4)))))
+(define derivatives
+  `((,(lambda (x) 1) . ,(lambda (x) 0))
+    (,(lambda (x) x) . ,(lambda (x) 1))
+    (,(lambda (x) (* 3 x)) . ,(lambda (x) 3))
+    (,(lambda (x) (expt x 3)) . ,(lambda (x) (* 3 (expt x 2))))))
 
 (test-case
  "integration"
  (let ((step 0.001))
-   (for (((f g) (in-dict integrated)))
+   (for (((f f-prime) (in-dict derivatives)))
      (for ((i (in-range 5)))
        (let* ((lo (random 50))
               (hi (random (add1 lo) 100))
-              (definite (- (g hi) (g lo)))
-              (tolerance (* 0.01 (abs definite))))
-         (check-= definite (simpson-integral f lo hi step) tolerance)
-         (check-= definite (riemann-integral f lo hi step) tolerance))))))
+              (definite-integral (- (f hi) (f lo)))
+              (tolerance (* 0.01 (abs definite-integral))))
+         (check-= definite-integral (simpson-integral f-prime lo hi step) tolerance)
+         (check-= definite-integral (riemann-integral f-prime lo hi step) tolerance))))))
--- ch2/57/test.rkt
+++ ch2/57/test.rkt
@@ -4,7 +4,7 @@
          rackunit
          "main.rkt")
 
-(define differentiated
+(define derivatives
   `((1 . 0)
     (x . 1)
     ((+ x x x) . 3)
@@ -12,8 +12,8 @@
 
 (test-case
  "differentiation"
- (for (((f g) (in-dict differentiated)))
+ (for (((f f-prime) (in-dict derivatives)))
    (for ((i (in-range 5)))
      (let* ((x (random -100 100)))
        (check-equal? (eval-deriv f 'x x)
-                     (eval g 'x x))))))
+                     (eval f-prime 'x x))))))
